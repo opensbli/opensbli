9cb7185dfb3edf185e468f6fd770fab0213e7aa3
satyajammy <s.p.jammy@soton.ac.uk>
2015-12-03 10:11:53 +0000

    3122015 starting version

--- run.sh
+++ run.sh
@@ -3,8 +3,10 @@ rm alg.tex equations.tex *.cpp *.h *.f90
 echo "Expanded equations waiting for 10 seconds"
 # sleep 2s
 echo "doing post stuff"
-astyle -s2 --max-code-length=60 auto_kernel.h
-astyle -s2 --max-code-length=60 OPSC_nssolver.cpp
+astyle -s2 --style=gnu   --max-code-length=80 --convert-tabs auto_kernel.h
+# astyle -s2 --style=allman --indent=spaces=2  --max-code-length=80 --break-after-logical auto_kernel.h
+# astyle -s2 --style=allman --indent=spaces=2  --max-code-length=80 --break-after-logical auto_kernel.h
+astyle -s2 --style=gnu  --max-code-length=80  OPSC_nssolver.cpp
 pdflatex alg.tex
 # pdflatex equations.tex
 rm *.orig
