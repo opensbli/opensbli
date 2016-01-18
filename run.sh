d45e5653053fb88bf4e8fb6e0ea05be96bfea8c2
Christian Jacobs <c.jacobs10@imperial.ac.uk>
2016-01-18 19:00:34 +0000

    Only post-process the .tex file if it exists.

--- run.sh
+++ run.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+EQUATIONS_FILE="equations.tex"
+
 echo ">>> Cleaning up any existing generated files..."
 rm -f alg.tex equations.tex *.cpp *.h *.f90
 echo ">>> Done."
@@ -8,13 +10,18 @@ echo ">>> Expanding equations..."
 ./expansion.py equations
 echo ">>> Done."
 
-echo ">>> LaTeX post-processing..."
-astyle -s2 --style=gnu   --max-code-length=80 --convert-tabs auto_kernel.h
-# astyle -s2 --style=allman --indent=spaces=2  --max-code-length=80 --break-after-logical auto_kernel.h
-# astyle -s2 --style=allman --indent=spaces=2  --max-code-length=80 --break-after-logical auto_kernel.h
-astyle -s2 --style=gnu  --max-code-length=80  OPSC_nssolver.cpp
-pdflatex equations.tex
-echo ">>> Done."
+if [ -f $EQUATIONS_FILE ];
+then
+   echo ">>> LaTeX post-processing..."
+   astyle -s2 --style=gnu --max-code-length=80 --convert-tabs auto_kernel.h
+   # astyle -s2 --style=allman --indent=spaces=2  --max-code-length=80 --break-after-logical auto_kernel.h
+   # astyle -s2 --style=allman --indent=spaces=2  --max-code-length=80 --break-after-logical auto_kernel.h
+   astyle -s2 --style=gnu --max-code-length=80 OPSC_nssolver.cpp
+   pdflatex $EQUATIONS_FILE
+   echo ">>> Done."
+else
+   echo "!!! Warning: No LaTeX file exists. Skipping LaTeX post-processing..."
+fi
 
 echo ">>> Cleaning up build files..."
 rm *.orig
