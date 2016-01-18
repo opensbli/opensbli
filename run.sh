859e7c3566355313e409cd027e249362101ae53d
Christian Jacobs <c.jacobs10@imperial.ac.uk>
2016-01-18 18:44:46 +0000

    Tidying up run.sh.

--- run.sh
+++ run.sh
@@ -1,15 +1,19 @@
-rm alg.tex equations.tex *.cpp *.h *.f90
-./expansion.py equations
-echo "Expanded equations waiting for 10 seconds"
+rm -f alg.tex equations.tex *.cpp *.h *.f90
+echo ">>> Expanding equations..."
+python expansion.py equations
+echo ">>> Done."
 # sleep 2s
-echo "doing post stuff"
+echo ">>> LaTeX post-processing..."
 astyle -s2 --style=gnu   --max-code-length=80 --convert-tabs auto_kernel.h
 # astyle -s2 --style=allman --indent=spaces=2  --max-code-length=80 --break-after-logical auto_kernel.h
 # astyle -s2 --style=allman --indent=spaces=2  --max-code-length=80 --break-after-logical auto_kernel.h
 astyle -s2 --style=gnu  --max-code-length=80  OPSC_nssolver.cpp
-pdflatex alg.tex
-# pdflatex equations.tex
+pdflatex equations.tex
+echo ">>> Done."
+
+echo ">>> Cleaning up build files..."
 rm *.orig
 rm *.pyc
 rm *.aux
 rm *.log
+echo ">>> Done."
