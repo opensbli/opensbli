cb07f5e7ed8d924e3b705b61dbf99483ae4f668a
Christian Jacobs <c.jacobs10@imperial.ac.uk>
2016-01-20 14:21:45 +0000

    Created a new function to read in the equation input file. Also tidied up the variable names.

--- autofd/expansion.py
+++ autofd/expansion.py
@@ -20,6 +20,35 @@ LOG = logging.getLogger(__name__)
 
 BUILD_DIR = os.getcwd()
 
+def read_input(equations_file):
+    """ Read the equations file and extract the equations and their parameters. 
+    
+    :arg str equations_file: The path to the equations file.
+    """
+
+    # Remove leading and trailing white spaces and empty lines
+    with open(equations_file) as f:
+        read_file = [line for line in f.read().splitlines() if line]
+    comment_lineno = [] # Line numbers of each (Python) comment line, which we want to ignore
+
+    # Get all the comments in the file
+    for ind,line in enumerate(read_file):
+        if line[0] == '#':
+            comment_lineno.append(ind)
+
+    inp = EquationInput() # Define the inputs
+
+    # Read inputs from the file
+    inp.equations = read_file[comment_lineno[0]+1:comment_lineno[1]]
+    inp.substitutions = read_file[comment_lineno[1]+1:comment_lineno[2]]
+    inp.ndim = int(read_file[comment_lineno[2]+1])
+    inp.constants = read_file[comment_lineno[3]+1:comment_lineno[4]]
+    inp.coordinate_symbol = read_file[comment_lineno[4]+1:comment_lineno[5]]
+    inp.metrics = read_file[comment_lineno[5]+1:comment_lineno[6]]
+    inp.formulas = read_file[comment_lineno[6]+1:comment_lineno[7]]
+
+    return inp
+
 def expand_equations(equations_file):
    """ Perform an expansion of the equations, provided by the user, and written in Einstein notation.
    
@@ -29,62 +58,44 @@ def expand_equations(equations_file):
    # Find out the path of the directory that the equations_file is in.
    base_path = os.path.dirname(equations_file)
    
-   # Remove leading and trailing white spaces and empty lines
-   with open(equations_file) as f:
-      read_file = [line for line in f.read().splitlines() if line]
-   comm_lineno = [] # Line numbers of each (Python) comment line, which we want to ignore
+   # Get the equation inputs
+   inp = read_input(equations_file)
 
-   # Get all the comments in the file
-   for ind,line in enumerate(read_file):
-     if line[0] == '#':
-       comm_lineno.append(ind)
-
-   inp = Inputs() # Define the inputs
-   
-   # Read inputs from the file
-   inp.eq = read_file[comm_lineno[0]+1:comm_lineno[1]]
-   inp.substi = read_file[comm_lineno[1]+1:comm_lineno[2]]
-   inp.ndim = int(read_file[comm_lineno[2]+1])
-   inp.const = read_file[comm_lineno[3]+1:comm_lineno[4]]
-   inp.coord = read_file[comm_lineno[4]+1:comm_lineno[5]]
-   inp.metric = read_file[comm_lineno[5]+1:comm_lineno[6]]
-   inp.formula = read_file[comm_lineno[6]+1:comm_lineno[7]]
    # Find the tensor indices in the equations
-
    # Expand the equations
    start = time.time()
-   eqs = []
-   for eq in inp.eq:
-     eqs.append(Equation(eq, inp))
+   equations = []
+   for e in inp.equations:
+     equations.append(Equation(e, inp))
    formulas = []
-   for formula in inp.formula:
-     formulas.append(Equation(formula, inp))
+   for f in inp.formulas:
+     formulas.append(Equation(f, inp))
    #for eq in eqs:
      #pprint(eq.expandedeq)
    end = time.time()
-   LOG.debug('The time taken for tensor expansion of equations in %d Dimensions is %s seconds.' % (inp.ndim, end - start))
+   LOG.debug('The time taken for tensor expansion of equations in %d Dimensions is %.2f seconds.' % (inp.ndim, end - start))
    
    # Prepare equations for algorithm
-   algorithm_file_path = base_path+"/algorithm"
+   algorithm_file_path = base_path + "/algorithm"
    read_file = [line for line in open(algorithm_file_path, "r").read().splitlines() if line]
-   alg = read_alg(read_file)
+   algorithm = read_alg(read_file)
    start = time.time()
-   final_equation = PreparedEquations(eqs, formulas, alg)
+   final_equation = PreparedEquations(equations, formulas, algorithm)
    end = time.time()
-   LOG.debug('The time taken for tensor expansion of equations in %d Dimensions is %s seconds.' % (inp.ndim, end - start))
+   LOG.debug('The time taken for tensor expansion of equations in %d Dimensions is %.2f seconds.' % (inp.ndim, end - start))
 
    # Output equations in LaTeX format.
    latex_file_path = BUILD_DIR + "/equations.tex"
    with open(latex_file_path, 'w') as latex_file:
       temp = []
-      for eq in eqs:
-        temp = temp + [eq.parsed]
+      for e in equations:
+        temp = temp + [e.parsed]
       inp = ['Equations', 'Satya P Jammy', 'University of Southampton']
-      header , end = latex_article_header(inp)
+      header, end = latex_article_header(inp)
       latex_file.write(header)
       temp = []
-      for eq in eqs:
-        temp = temp + [eq.expandedeq]
+      for e in equations:
+        temp = temp + [e.expandedeq]
       temp = flatten(temp)
       write_latex(latex_file, temp)
       latex_file.write(end)
