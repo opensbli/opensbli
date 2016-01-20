fd16e4ae93bd2908de1ee01a09afa79419e03100
Christian Jacobs <c.jacobs10@imperial.ac.uk>
2016-01-20 14:26:27 +0000

    Added the start of a test suite. This tests the new read_input function.

---
+++ tests/test_expansion.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+import os
+import pytest
+
+# AutoFD functions
+from autofd.expansion import *
+
+def test_read_input():
+    """ Ensure that the equation inputs are read correctly. """
+
+    i = read_input("data/equations")
+    
+    # Check equations
+    assert len(i.equations) == 3
+    assert i.equations == ["Eq(Der(rho,t),- conser(rhou_j,x_j))", "Eq(Der(rhou_i,t) ,-conser(rhou_i*u_j + p* KroneckerDelta(_i,_j),x_j) + Der(tau_i_j,x_j) )", "Eq(Der(rhoE,t),-conser((p+rhoE)*u_j,x_j) -Der(q_i,x_i) + Der(u_i*tau_i_j ,x_j) )"]
+
+
+if __name__ == '__main__':
+    pytest.main(os.path.abspath(__file__))
