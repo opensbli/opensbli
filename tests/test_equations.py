b6535ffdca7da59e3ee6ba7916a0b98b0237eab1
Christian T. Jacobs <C.T.Jacobs@soton.ac.uk>
2016-02-08 16:15:21 +0000

    Fix comments.

--- tests/test_equations.py
+++ tests/test_equations.py
@@ -7,8 +7,8 @@ import pytest
 from autofd.problem import *
 from autofd.equations import *
 
-def test_expand():
-    """ Ensure that an equation is expanded correctly. """
+def test_equations_to_dict():
+    """ Ensure that a list of Sympy Eq objects is converted to a dictionary. """
 
     mass = "Eq(Der(rho,t),- conser(rhou_j,x_j))"
     momentum = "Eq(Der(rhou_i,t) ,-conser(rhou_i*u_j + p* KroneckerDelta(_i,_j),x_j) + Der(tau_i_j,x_j) )"
