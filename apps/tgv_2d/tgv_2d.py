8292505df06c04ee4e12691c50cff1d690a70a25
Christian T. Jacobs <C.T.Jacobs@soton.ac.uk>
2016-02-17 10:14:03 +0000

    Recovered the instantiation of System and Algorithm in tgv_2d.py.

--- apps/tgv_2d/tgv_2d.py
+++ apps/tgv_2d/tgv_2d.py
@@ -51,3 +51,33 @@ latex.write_equations(temp)
 latex.write_footer()
 latex.close()
 
+# Prepare the algorithm
+temporal_scheme = "RK" # Runge-Kutta time-stepping scheme
+temporal_order = 3
+constant_timestep = True
+spatial_scheme = "central_diff"
+spatial_order = 4
+evaluation_count = 0
+work_array = "wk"
+bcs = [("periodic", "periodic")]*ndim # Boundary conditions. The tuple represents (left, right) ends, and there is one tuple per dimension.
+language = "OPSC"
+multiblock = False
+explicit_filter = [False]*ndim
+
+algorithm = Algorithm(temporal_scheme, temporal_order, constant_timestep, spatial_scheme, spatial_order, work_array, evaluation_count, bcs, language, multiblock, explicit_filter)
+
+# Prepare the computational system and generate the code
+start = time.time()
+system = System()
+
+Lx0 = "2.0*M_PI"
+Lx1 = "2.0*M_PI"
+simulation_parameters = {"name":"tgv_2d", "Lx0":Lx0, "Lx1":Lx1, "nx0p[blk]":"32", "nx1p[blk]":"32", "dt":"0.0005", "niter":"1000", "Minf":"0.1", "mu":"1.0", "a1":["2.0/3.0", "5.0/12.0", "3.0/5.0"], "a2":["1.0/4.0", "3.0/20.0", "3.0/5.0"], "gama":"1.4", "niter":"100", "Pr":"0.71", "Re":"1600", "dx0":"%s/nx0p[blk]" % Lx0, "dx1":"%s/nx1p[blk]" % Lx1}
+
+system.prepare(expanded_equations, expanded_formulas, algorithm, simulation_parameters)
+end = time.time()
+LOG.debug('The time taken to prepare the system in %d Dimensions is %.2f seconds.' % (problem.ndim, end - start))
+
+# Translate and compile the generated code
+LOG.info("Compiling generated code...")
+system.compile(algorithm, simulation_parameters)
