3c39c62e018637091f1faeb42ac74d787dc7713a
Christian T. Jacobs <C.T.Jacobs@soton.ac.uk>
2016-02-17 11:49:34 +0000

    Create fixtures and test more EinsteinTerm attributes.

--- tests/test_equations.py
+++ tests/test_equations.py
@@ -3,29 +3,56 @@
 import os
 import pytest
 
-# AutoFD functions
-from autofd.problem import *
 from autofd.equations import *
+
 
 @pytest.fixture
 def mass():
     return Equation("Eq(Der(rho,t), -Conservative(rhou_j,x_j))", 2, substitutions=[], constants=[])
 
 
-def test_indices():
+@pytest.fixture
+def c():
+    """ A scalar EinsteinTerm """
+    return EinsteinTerm("c")
+
+
+@pytest.fixture
+def u_j():
+    """ A vector EinsteinTerm """
+    return EinsteinTerm("u_j")
+
+
+@pytest.fixture
+def tau_i_j():
+    """ A tensor EinsteinTerm """
+    return EinsteinTerm("tau_i_j")
+
+
+def test_indices(c, u_j, tau_i_j):
     """ Check that all indices of an Einstein term are parsed correctly. """
 
-    assert EinsteinTerm("c").indices == []  # Scalar
-    assert EinsteinTerm("u_j").indices == ["_j"]  # Vector
-    assert EinsteinTerm("tau_i_j").indices == ["_i", "_j"]  # Tensor
+    assert c.indices == []  # Scalar
+    assert u_j.indices == ["_j"]  # Vector
+    assert tau_i_j.indices == ["_i", "_j"]  # Tensor
 
 
-def test_name():
+def test_name(c, u_j, tau_i_j):
     """ Check that the name of an Einstein term is correct. """
 
-    assert EinsteinTerm("c").name == "c"  # Scalar
-    assert EinsteinTerm("u_j").name == "u_j"  # Vector
-    assert EinsteinTerm("tau_i_j").name == "tau_i_j"  # Tensor
+    assert c.name == "c"  # Scalar
+    assert u_j.name == "u_j"  # Vector
+    assert tau_i_j.name == "tau_i_j"  # Tensor
+
+def test_has_index(u_j, tau_i_j):
+    """ Check that the name of an Einstein term is correct. """
+
+    assert u_j.has_index("_j")
+    assert not u_j.has_index("_i")
+
+    assert tau_i_j.has_index("_i")
+    assert tau_i_j.has_index("_j")
+    assert not tau_i_j.has_index("_k")
 
 
 def test_equations_to_dict(mass):
