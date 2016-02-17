4d58f4e127a785bb3772e353bb31104fa63a7abb
Christian T. Jacobs <C.T.Jacobs@soton.ac.uk>
2016-02-17 11:38:11 +0000

    Moar tests.

--- tests/test_equations.py
+++ tests/test_equations.py
@@ -7,23 +7,29 @@ import pytest
 from autofd.problem import *
 from autofd.equations import *
 
-def test_EinsteinTerm():
+@pytest.fixture
+def mass():
+    return Equation("Eq(Der(rho,t), -Conservative(rhou_j,x_j))", 2, substitutions=[], constants=[])
+
+def test_einstein_indices():
     """ Check that all indices of an Einstein term are parsed correctly. """
+
+    term = EinsteinTerm("c")
+    assert term.indices == []
+
     term = EinsteinTerm("u_j")
     assert term.indices == ["_j"]
 
+    term = EinsteinTerm("tau_i_j")
+    assert term.indices == ["_i", "_j"]
 
-def test_equations_to_dict():
+def test_equations_to_dict(mass):
     """ Ensure that a list of Sympy Eq objects is converted to a dictionary. """
 
-    mass = "Eq(Der(rho,t), -Conservative(rhou_j,x_j))"
-    momentum = "Eq(Der(rhou_i,t), -Conservative(rhou_i*u_j + p* KroneckerDelta(_i,_j),x_j) + Der(tau_i_j,x_j) )"
-    energy = "Eq(Der(rhoE,t), -Conservative((p+rhoE)*u_j,x_j) -Der(q_i,x_i) + Der(u_i*tau_i_j ,x_j) )"
-    equations = [mass, momentum, energy]
-
-    d = equations_to_dict([e.expanded[0] for e in expanded_equations])
-
-    assert isinstance(d, dict)
+    equations = [mass.expanded[0]]
+    assert isinstance(equations, list)
+    equations = equations_to_dict(equations)
+    assert isinstance(equations, dict)
 
     return
 
