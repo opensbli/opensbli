ae37a4a7cd1aaf1c01fdf5dab40c5882bc0cef37
Christian T. Jacobs <C.T.Jacobs@soton.ac.uk>
2016-03-11 11:16:57 +0000

    Fixed the equations.py tests following the recent changes.

--- tests/test_equations.py
+++ tests/test_equations.py
@@ -3,17 +3,22 @@
 import os
 import pytest
 
+from sympy import Symbol, Idx
+
 from autofd.equations import *
 
+@pytest.fixture
+def coordinate_symbol():
+    return "x"
 
 @pytest.fixture
-def mass():
-    return Equation("Eq(Der(rho,t), -Conservative(rhou_i,x_j))", 3, substitutions=[], constants=[])
+def mass(coordinate_symbol):
+    return Equation("Eq(Der(rho,t), -Conservative(rhou_j,x_j))", 3, coordinate_symbol, substitutions=[], constants=[])
 
 
 @pytest.fixture
 def stress_tensor():
-    return "Eq(tau_i_j, (mu/Re)*(Der(u_i,x_j) + Der(u_j,x_i) - (2/3) * KroneckerDelta(_i,_j)*Der(u_k,x_k)))"
+    return "Eq(tau_i_j, (mu/Re)*(Der(u_i,x_j) + Der(u_j,x_i) - (2/3) * KD(_i,_j)*Der(u_k,x_k)))"
 
 
 @pytest.fixture
@@ -22,13 +27,13 @@ def heat_flux():
 
 
 @pytest.fixture
-def momentum(stress_tensor):
-    return Equation("Eq(Der(rhou_i,t) ,-Conservative(rhou_i*u_j + p* KroneckerDelta(_i,_j),x_j) + Der(tau_i_j,x_j) )", 3, substitutions=[stress_tensor], constants=["Re", "mu"])
+def momentum(stress_tensor, coordinate_symbol):
+    return Equation("Eq(Der(rhou_i,t) ,-Conservative(rhou_i*u_j + p* KD(_i,_j),x_j) + Der(tau_i_j,x_j) )", 3, coordinate_symbol, substitutions=[stress_tensor], constants=["Re", "mu"])
 
 
 @pytest.fixture
-def energy(stress_tensor, heat_flux):
-    return Equation("Eq(Der(rhoE,t),-Conservative((p+rhoE)*u_j,x_j) -Der(q_i,x_i) + Der(u_i*tau_i_j ,x_j) )", 3, substitutions=[stress_tensor, heat_flux], constants=["Re", "Pr", "mu", "gama", "Minf"])
+def energy(stress_tensor, heat_flux, coordinate_symbol):
+    return Equation("Eq(Der(rhoE,t),-Conservative((p+rhoE)*u_j,x_j) -Der(q_i,x_i) + Der(u_i*tau_i_j ,x_j) )", 3, coordinate_symbol, substitutions=[stress_tensor, heat_flux], constants=["Re", "Pr", "mu", "gama", "Minf"])
 
 
 @pytest.fixture
@@ -57,104 +62,42 @@ def tau_i_j():
     return EinsteinTerm("tau_i_j")
 
 
-def test_indices(c, u_i, tau_i_j):
-    """ Check that all indices of an Einstein term are parsed correctly. """
+def test_get_indices(c, u_i, tau_i_j):
+    """ Check that all indices of an EinsteinTerm are parsed correctly. """
 
-    assert c.indices == []  # Scalar
-    assert u_i.indices == ["_i"]  # Vector
-    assert tau_i_j.indices == ["_i", "_j"]  # Tensor
+    assert c.get_indices() == []  # Scalar
+    assert u_i.get_indices() == [Idx(Symbol("i", integer=True))]  # Vector
+    assert tau_i_j.get_indices() == [Idx(Symbol("i", integer=True)), Idx(Symbol("j", integer=True))]  # Tensor
 
 
 def test_name(c, u_i, tau_i_j):
-    """ Check that the name of an Einstein term is correct. """
+    """ Check that the base name of an EinsteinTerm is correct. """
 
-    assert c.name == "c"  # Scalar
-    assert u_i.name == "u_i"  # Vector
-    assert tau_i_j.name == "tau_i_j"  # Tensor
-
-
-def test_has_index(c, u_i, tau_i_j):
-    """ Check that various Einstein terms have the indices that they should have. """
-
-    assert not c.has_index("_l")
-
-    assert u_i.has_index("_i")
-    assert not u_i.has_index("_j")
-
-    assert tau_i_j.has_index("_i")
-    assert tau_i_j.has_index("_j")
-    assert not tau_i_j.has_index("_k")
-
-
-def test_expand_index(c_constant, u_i, tau_i_j):
-    """ Check that an index can successfully be applied to an Einstein term. """
-
-    new = c_constant.expand_index("_i", 0)
-    assert new == c_constant  # This should stay the same since the Einstein term is constant.
-
-    new = u_i.expand_index("_i", 0)
-    assert not new.has_index("_i")
-
-    new = tau_i_j.expand_index("_i", 0)
-    assert new.has_index("_j")
-    assert not new.has_index("_i")
+    assert c.get_base() == "c"  # Scalar
+    assert u_i.get_base() == "u"  # Vector
+    assert tau_i_j.get_base() == "tau"  # Tensor
 
 
 def test_equation_expand(mass, momentum, energy):
     """ Check that the Navier-Stokes equations are expanded correctly. """
 
     # Conservation of mass
-    assert str(mass.parsed) == "Der(rho, t) == -Conservative(rhou_i, x_j)"
+    assert str(mass.parsed) == "Der(rho, t) == -Conservative(rhou_j, x_j)"
     assert isinstance(mass.expanded, list)
     assert len(mass.expanded) == 1
-    assert str(mass.expanded[0]) == "Derivative(rho(t, x0, x1, x2), t) == -Derivative(rhou0(t, x0, x1, x2), x0) - Derivative(rhou0(t, x0, x1, x2), x1) - Derivative(rhou0(t, x0, x1, x2), x2) - Derivative(rhou1(t, x0, x1, x2), x0) - Derivative(rhou1(t, x0, x1, x2), x1) - Derivative(rhou1(t, x0, x1, x2), x2) - Derivative(rhou2(t, x0, x1, x2), x0) - Derivative(rhou2(t, x0, x1, x2), x1) - Derivative(rhou2(t, x0, x1, x2), x2)"
-    assert str(mass.expanded[0]).count("Derivative") == 10  # 1 time Derivative, then 3 Derivative for each of the 3 dimensions
-
-    # Conservation of momentum
-    assert str(momentum.parsed) == "Der(rhou_i, t) == -Conservative(p*KroneckerDelta(_i, _j) + rhou_i*u_j, x_j) + Der(mu*(Der(u_i, x_j) + Der(u_j, x_i) - 2*Der(u_k, x_k)*KroneckerDelta(_i, _j)/3)/Re, x_j)"
-    assert isinstance(momentum.expanded, list)
-    assert len(momentum.expanded) == 3  # Velocity is a vector, and the problem is 3D, so we should get 3 equations back (one for each component of the vector).
-    assert str(momentum.expanded[0]) == "Derivative(rhou0(t, x0, x1, x2), t) == -Derivative(rhou0(t, x0, x1, x2)*u1(t, x0, x1, x2), x1) - Derivative(rhou0(t, x0, x1, x2)*u2(t, x0, x1, x2), x2) - Derivative(p(t, x0, x1, x2) + rhou0(t, x0, x1, x2)*u0(t, x0, x1, x2), x0) + mu*(Derivative(u0(t, x0, x1, x2), x1, x1) + Derivative(u1(t, x0, x1, x2), x0, x1))/Re + mu*(Derivative(u0(t, x0, x1, x2), x2, x2) + Derivative(u2(t, x0, x1, x2), x0, x2))/Re + mu*(4*Derivative(u0(t, x0, x1, x2), x0, x0)/3 - 2*Derivative(u1(t, x0, x1, x2), x0, x1)/3 - 2*Derivative(u2(t, x0, x1, x2), x0, x2)/3)/Re"
-    assert str(momentum.expanded[1]) == "Derivative(rhou1(t, x0, x1, x2), t) == -Derivative(rhou1(t, x0, x1, x2)*u0(t, x0, x1, x2), x0) - Derivative(rhou1(t, x0, x1, x2)*u2(t, x0, x1, x2), x2) - Derivative(p(t, x0, x1, x2) + rhou1(t, x0, x1, x2)*u1(t, x0, x1, x2), x1) + mu*(Derivative(u0(t, x0, x1, x2), x0, x1) + Derivative(u1(t, x0, x1, x2), x0, x0))/Re + mu*(Derivative(u1(t, x0, x1, x2), x2, x2) + Derivative(u2(t, x0, x1, x2), x1, x2))/Re + mu*(-2*Derivative(u0(t, x0, x1, x2), x0, x1)/3 + 4*Derivative(u1(t, x0, x1, x2), x1, x1)/3 - 2*Derivative(u2(t, x0, x1, x2), x1, x2)/3)/Re"
-    assert str(momentum.expanded[2]) == "Derivative(rhou2(t, x0, x1, x2), t) == -Derivative(rhou2(t, x0, x1, x2)*u0(t, x0, x1, x2), x0) - Derivative(rhou2(t, x0, x1, x2)*u1(t, x0, x1, x2), x1) - Derivative(p(t, x0, x1, x2) + rhou2(t, x0, x1, x2)*u2(t, x0, x1, x2), x2) + mu*(Derivative(u0(t, x0, x1, x2), x0, x2) + Derivative(u2(t, x0, x1, x2), x0, x0))/Re + mu*(Derivative(u1(t, x0, x1, x2), x1, x2) + Derivative(u2(t, x0, x1, x2), x1, x1))/Re + mu*(-2*Derivative(u0(t, x0, x1, x2), x0, x2)/3 - 2*Derivative(u1(t, x0, x1, x2), x1, x2)/3 + 4*Derivative(u2(t, x0, x1, x2), x2, x2)/3)/Re"
-
-    # Conservation of energy
-    print energy.parsed
-    print str(energy.expanded[0]) 
-    assert str(energy.parsed) == "Der(rhoE, t) == -Conservative(u_j*(p + rhoE), x_j) + Der(mu*u_i*(Der(u_i, x_j) + Der(u_j, x_i) - 2*Der(u_k, x_k)*KroneckerDelta(_i, _j)/3)/Re, x_j) - Der(-mu*Der(T, x_i)/(Minf**2*Pr*Re*(gama - 1)), x_i)"
-    assert isinstance(energy.expanded, list)
-    assert len(energy.expanded) == 1
-    assert str(energy.expanded[0]) == "Derivative(rhoE(t, x0, x1, x2), t) == -Derivative((p(t, x0, x1, x2) + rhoE(t, x0, x1, x2))*u0(t, x0, x1, x2), x0) - Derivative((p(t, x0, x1, x2) + rhoE(t, x0, x1, x2))*u1(t, x0, x1, x2), x1) - Derivative((p(t, x0, x1, x2) + rhoE(t, x0, x1, x2))*u2(t, x0, x1, x2), x2) + mu*((Derivative(u0(t, x0, x1, x2), x1) + Derivative(u1(t, x0, x1, x2), x0))*Derivative(u0(t, x0, x1, x2), x1) + (Derivative(u1(t, x0, x1, x2), x2) + Derivative(u2(t, x0, x1, x2), x1))*Derivative(u2(t, x0, x1, x2), x1) + (Derivative(u0(t, x0, x1, x2), x1, x1) + Derivative(u1(t, x0, x1, x2), x0, x1))*u0(t, x0, x1, x2) + (Derivative(u1(t, x0, x1, x2), x1, x2) + Derivative(u2(t, x0, x1, x2), x1, x1))*u2(t, x0, x1, x2) + (-2*Derivative(u0(t, x0, x1, x2), x0)/3 + 4*Derivative(u1(t, x0, x1, x2), x1)/3 - 2*Derivative(u2(t, x0, x1, x2), x2)/3)*Derivative(u1(t, x0, x1, x2), x1) + (-2*Derivative(u0(t, x0, x1, x2), x0, x1)/3 + 4*Derivative(u1(t, x0, x1, x2), x1, x1)/3 - 2*Derivative(u2(t, x0, x1, x2), x1, x2)/3)*u1(t, x0, x1, x2))/Re + mu*((Derivative(u0(t, x0, x1, x2), x1) + Derivative(u1(t, x0, x1, x2), x0))*Derivative(u1(t, x0, x1, x2), x0) + (Derivative(u0(t, x0, x1, x2), x2) + Derivative(u2(t, x0, x1, x2), x0))*Derivative(u2(t, x0, x1, x2), x0) + (Derivative(u0(t, x0, x1, x2), x0, x1) + Derivative(u1(t, x0, x1, x2), x0, x0))*u1(t, x0, x1, x2) + (Derivative(u0(t, x0, x1, x2), x0, x2) + Derivative(u2(t, x0, x1, x2), x0, x0))*u2(t, x0, x1, x2) + (4*Derivative(u0(t, x0, x1, x2), x0)/3 - 2*Derivative(u1(t, x0, x1, x2), x1)/3 - 2*Derivative(u2(t, x0, x1, x2), x2)/3)*Derivative(u0(t, x0, x1, x2), x0) + (4*Derivative(u0(t, x0, x1, x2), x0, x0)/3 - 2*Derivative(u1(t, x0, x1, x2), x0, x1)/3 - 2*Derivative(u2(t, x0, x1, x2), x0, x2)/3)*u0(t, x0, x1, x2))/Re + mu*((Derivative(u0(t, x0, x1, x2), x2) + Derivative(u2(t, x0, x1, x2), x0))*Derivative(u0(t, x0, x1, x2), x2) + (Derivative(u1(t, x0, x1, x2), x2) + Derivative(u2(t, x0, x1, x2), x1))*Derivative(u1(t, x0, x1, x2), x2) + (Derivative(u0(t, x0, x1, x2), x2, x2) + Derivative(u2(t, x0, x1, x2), x0, x2))*u0(t, x0, x1, x2) + (Derivative(u1(t, x0, x1, x2), x2, x2) + Derivative(u2(t, x0, x1, x2), x1, x2))*u1(t, x0, x1, x2) + (-2*Derivative(u0(t, x0, x1, x2), x0)/3 - 2*Derivative(u1(t, x0, x1, x2), x1)/3 + 4*Derivative(u2(t, x0, x1, x2), x2)/3)*Derivative(u2(t, x0, x1, x2), x2) + (-2*Derivative(u0(t, x0, x1, x2), x0, x2)/3 - 2*Derivative(u1(t, x0, x1, x2), x1, x2)/3 + 4*Derivative(u2(t, x0, x1, x2), x2, x2)/3)*u2(t, x0, x1, x2))/Re + mu*Derivative(T(t, x0, x1, x2), x0, x0)/(Minf**2*Pr*Re*(gama - 1)) + mu*Derivative(T(t, x0, x1, x2), x1, x1)/(Minf**2*Pr*Re*(gama - 1)) + mu*Derivative(T(t, x0, x1, x2), x2, x2)/(Minf**2*Pr*Re*(gama - 1))"
+    assert str(mass.expanded[0]) == "Derivative(rho[x0, x1, x2, t], t) == -Derivative(rhou0[x0, x1, x2, t], x0) - Derivative(rhou1[x0, x1, x2, t], x1) - Derivative(rhou2[x0, x1, x2, t], x2)"
+    assert str(mass.expanded[0]).count("Derivative") == 4  # 1 time Derivative, then 3 Derivative for each dimension
 
 
-def test_has_nested_derivative():
-    """ Ensure that nested derivatives (Der) objects are correctly found. """
 
-    ndim = 2
-    equation = Equation("Eq(Der(rhoE,t), Der(u_i*Der(u_i,x_j),x_j) )", ndim)
-    expansion = EinsteinExpansion(equation.parsed, ndim)
-    assert expansion.has_nested_derivative(equation.parsed.rhs)
-    assert not expansion.has_nested_derivative(equation.parsed.lhs)
-    
-
-def test_get_nested():
-    """ Ensure that all terms (whole tree nodes) with index "_i" are identified correctly. """
-    ndim = 2
-    equation = Equation("Eq(Der(rhoE,t), Der(u_i*Der(u_i,x_j),x_j) )", ndim)
-    expansion = EinsteinExpansion(equation.parsed, ndim)
-    nested_terms = expansion.get_nested(equation.parsed.rhs, "_i")
-    assert len(nested_terms) == 1
-    assert str(nested_terms[0]) == "u_i*Der(u_i, x_j)"
-
-
-def test_get_einstein_indices():
-    """ Get all the Einstein indices in the equation's RHS. """
-    ndim = 2
-    equation = Equation("Eq(Der(rhoE,t), Der(u_i*Der(u_i,x_j),x_j) )", ndim)
-    expansion = EinsteinExpansion(equation.parsed, ndim)
-    indices = expansion.get_einstein_indices(expression=equation.parsed.rhs)
-    assert len(indices) == 2
-    assert indices == set(["_i", "_j"])
+#def test_get_einstein_indices():
+#    """ Get all the Einstein indices in the equation's RHS. """
+#    ndim = 2
+#    equation = Equation("Eq(Der(rhoE,t), Der(u_i*Der(u_i,x_j),x_j) )", ndim)
+#    expansion = EinsteinExpansion(equation.parsed, ndim)
+#    indices = expansion.get_einstein_indices(expression=equation.parsed.rhs)
+#    assert len(indices) == 2
+#    assert indices == set(["_i", "_j"])
 
 
 if __name__ == '__main__':
