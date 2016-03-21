0a2feb2cd29e82e84cac211ba85249d562d7b116
Christian T. Jacobs <C.T.Jacobs@soton.ac.uk>
2016-03-21 14:05:32 +0000

    Moar docstrings.

--- autofd/scheme.py
+++ autofd/scheme.py
@@ -1,5 +1,14 @@
 class Scheme(object):
+
+    """ A numerical discretisation scheme. """
+
     def __init__(self, name, order):
+        """ Initialise the scheme.
+        
+        :arg str name: The name of the scheme.
+        :arg int order: The order of the scheme.
+        """
+        
         self.name = name
         self.order = order
         return
