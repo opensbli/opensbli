033be5d2d89045903cb600ed0c72f8a92beeda18
Christian T. Jacobs <C.T.Jacobs@soton.ac.uk>
2016-03-23 11:08:07 +0000

    More copyright statements.

--- opensbli/scheme.py
+++ opensbli/scheme.py
@@ -1,3 +1,23 @@
+#!/usr/bin/env python
+
+#    OpenSBLI: An automatic code generator for solving differential equations.
+#    Copyright (C) 2016 Satya P. Jammy, Christian T. Jacobs
+
+#    This file is part of OpenSBLI.
+
+#    OpenSBLI is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+
+#    OpenSBLI is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+
+#    You should have received a copy of the GNU General Public License
+#    along with OpenSBLI.  If not, see <http://www.gnu.org/licenses/>
+
 class Scheme(object):
 
     """ A numerical discretisation scheme. """
