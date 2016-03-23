c849483e6151f5821c4e522c7ef88c42a7290f12
Christian T. Jacobs <C.T.Jacobs@soton.ac.uk>
2016-03-23 11:15:08 +0000

    Moar Grid tests.

--- tests/test_grid.py
+++ tests/test_grid.py
@@ -12,14 +12,21 @@ from opensbli.grid import *
 def grid():
     return Grid(ndim=3)
 
+@pytest.fixture
+def grid_with_data():
+    return Grid(ndim=3, grid_data={"number_of_points":[10, 5, 2], "delta":[0.5, 0.25, 1.0]})
+    
 
-def test_grid(grid):
+def test_grid(grid, grid_with_data):
     """ Ensure that a numerical grid of solution points is set up correctly. """
 
     assert len(grid.shape) == 3
     assert grid.shape == tuple(symbols('nx0:3', integer=True)) # nx x ny x nz grid shape
     assert grid.indices == tuple(symbols('i0:3', integer=True)) # Grid index for each dimension
     assert len(grid.deltas) == 3
+
+    assert grid_with_data.shape == tuple([10, 5, 2]) # Grid index for each dimension
+    assert grid_with_data.deltas == [0.5, 0.25, 1.0]
     
     return
 
