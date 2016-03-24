cf1874dbfbd8620d1e2750fdc767fab0017a582e
Christian T. Jacobs <C.T.Jacobs@soton.ac.uk>
2016-03-24 15:33:34 +0000

    Compute the error in the solution field and write out the plot to a PDF file.

--- apps/wave/plot.py
+++ apps/wave/plot.py
@@ -28,18 +28,21 @@ def plot(path):
     
     # Coordinate array
     x = numpy.zeros(nx)
+    phi_analytical = numpy.zeros(nx)
+    phi_error = numpy.zeros(nx)
 
     # Compute the error
     for i in range(0, nx):
         x[i] = i*dx
         # Analytical solution
-        #u_analytical[i,j] = cos(x[i,j])*sin(y[i,j])*exp(-2*nu*t)
-        #v_analytical[i,j] = -sin(x[i,j])*cos(y[i,j])*exp(-2*nu*t)
-        #u_error[i,j] = u[i,j] - u_analytical[i,j]
-        #v_error[i,j] = v[i,j] - v_analytical[i,j]
-    print x
-    print phi
-    plt.plot(x, phi)
+        phi_analytical[i] = abs(sin(pi*(x[i]+0.5))) # Phi should be a sin wave shifted to the right by x = 0.5 (since the wave speed is 0.5 m/s and we've simulated until T = 1.0 s).
+        phi_error[i] = abs(phi_analytical[i] - phi[i])
+
+    plt.plot(x, phi_error, label=r"Absolute error in $\phi$")
+    plt.xlabel(r"$x$ (m)")
+    plt.ylabel(r"$\phi$")
+    plt.legend()
+    plt.savefig("phi_error.pdf")
     plt.show()
 
 
