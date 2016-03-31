c45cb288150238ef8d9589b137745c7a2a7b3190
Christian T. Jacobs <C.T.Jacobs@soton.ac.uk>
2016-03-31 15:59:30 +0100

    Added a 'run.py' file to setup and run a suite of MMS tests with varying grid resolutions.

--- apps/mms/error.py
+++ apps/mms/error.py
@@ -6,25 +6,25 @@ from math import pi, exp, cos, sin
 import matplotlib.pyplot as plt
 import h5py
 
-def error(path):
-    # Number of grid points
-    nx = 10
-    ny = 10
+def error(path, i, number_of_points):
+    # Number of grid points. This is assumed to be the same in the x and y directions.
+    nx = number_of_points
+    ny = number_of_points
     
     # Number of halo nodes at each end
     halo = 2
 
     # Read in the simulation output
     f = h5py.File(path + "/state.h5", 'r')
-    group = f["mms_block"]
+    group = f["mms_%d_block" % i]
     
     # Get the numerical solution field
     phi = group["phi"].value
     
     # Ignore the 2 halo nodes at either end of the domain
     phi = phi[halo:nx+halo, halo:ny+halo]
-    print phi.shape
-    print phi
+    #print phi.shape
+    #print phi
 
     # Grid spacing
     dx = (2.0*pi)/(nx)
@@ -45,8 +45,17 @@ def error(path):
             phi_analytical[i,j] = sin(x[i,j])
             phi_error[i,j] = abs(phi[i,j] - phi_analytical[i,j])
     
-    return numpy.linalg.norm(phi_error, order=2)
+    return numpy.linalg.norm(phi_error, ord=2)
 
+def plot(path):
+    
+    errors = []
+    
+    for i in range(0, 5):
+        number_of_points = 10*(2**i)
+        errors.append(error(path + "/mms_%d/mms_%d_opsc_code/" % (i, i), i, number_of_points))
+    
+    print errors
 
 if(__name__ == "__main__"):
     # Parse the command line arguments provided by the user
@@ -54,4 +63,4 @@ if(__name__ == "__main__"):
     parser.add_argument("path", help="The path to the directory containing the output files.", action="store", type=str)
     args = parser.parse_args()
     
-    print "The error is %f" % error(args.path)
+    plot(args.path)
