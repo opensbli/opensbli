90a67488fafa8c8fec63ac4ed9d98a51ad531143
Christian T. Jacobs <C.T.Jacobs@soton.ac.uk>
2016-04-06 22:47:43 +0100

    Updated the TGV test case using dimensional form.

--- apps/taylor_green_vortex/taylor_green_vortex.py
+++ apps/taylor_green_vortex/taylor_green_vortex.py
@@ -32,12 +32,12 @@ energy = "Eq(Der(rhoE,t),- Conservative((p+rhoE)*u_j,x_j) +Der(q_j,x_j) + Der(u_
 equations = [mass, momentum, energy]
 
 # Substitutions
-stress_tensor = "Eq(tau_i_j, (mu/Re)*(Der(u_i,x_j)+ Conservative(u_j,x_i)- (2/3)* KD(_i,_j)* Der(u_k,x_k)))"
-heat_flux = "Eq(q_j,  (mu/((gama-1)*Minf*Minf*Pr*Re))*Der(T,x_j))"
+stress_tensor = "Eq(tau_i_j, mu*(Der(u_i,x_j)+ Conservative(u_j,x_i)- (2/3)* KD(_i,_j)* Der(u_k,x_k)))"
+heat_flux = "Eq(q_j,  k*Der(T,x_j))"
 substitutions = [stress_tensor, heat_flux]
 
 # Define all the constants in the equations
-constants = ["Re", "Pr", "gama", "mu", "Minf", "C23"]
+constants = ["gama", "mu", "k", "cv"]
 
 # Coordinate direction symbol (x) this will be x_i, x_j, x_k
 coordinate_symbol = "x"
@@ -48,7 +48,7 @@ metrics = [False, False, False]
 # Formulas for the variables used in the equations
 velocity = "Eq(u_i, rhou_i/rho)"
 pressure = "Eq(p, (gama-1)*(rhoE/rho - (1/2)*(u_j*u_j)))"
-temperature = "Eq(T, p*gama*Minf*Minf/(rho))"
+temperature = "Eq(T, p/((gama-1)*rho*cv))"
 viscosity = "Eq(mu, T**(2/3))"
 formulas = [velocity, pressure, temperature, viscosity]
 
@@ -100,10 +100,17 @@ T0 = 300.0
 x = "(grid.Idx[0]*grid.deltas[0])"
 y = "(grid.Idx[1]*grid.deltas[1])"
 z = "(grid.Idx[2]*grid.deltas[2])"
+rhou0 = "(%f*sin(%s)*cos(%s)*cos(%s))" % (u0, x, y, z)
+rhou1 = "(-%f*cos(%s)*sin(%s)*cos(%s))" % (u0, x, y, z)
+p = "%f + (1.0/16.0)*(cos(2*%s) + cos(2*%s))*(cos(2*%s) + 2.0)" % (p0, x, y, z)
+ke = "0.5*(%s*%s + %s*%s)" % (rhou0, rhou0, rhou1, rhou1)
+rhoE = "%s/(1.4-1.0) + %s" % (p, ke)
+
 initial_conditions = ["Eq(grid.work_array(rho), 1.0)",
                       "Eq(grid.work_array(rhou0), %f*sin(%s)*cos(%s)*cos(%s))" % (u0, x, y, z),
                       "Eq(grid.work_array(rhou1), -%f*cos(%s)*sin(%s)*cos(%s))" % (u0, x, y, z),
                       "Eq(grid.work_array(rhou2), 0)",
+                      "Eq(grid.work_array(rhoE), %s)" % rhoE,
                       "Eq(grid.work_array(p), %f + (1.0/16.0)*(cos(2*%s) + cos(2*%s))*(cos(2*%s) + 2.0))" % (p0, x, y, z),
                       "Eq(grid.work_array(T), %f)" % (T0)]
 initial_conditions = GridBasedInitialisation(grid, initial_conditions)
@@ -113,15 +120,14 @@ io = FileIO(temporal_discretisation.prognostic_variables)
 
 # Grid parameters like number of points, length in each direction, and delta in each direction
 deltat = 3.385e-3
-T = 10.0
+T = 5.0#10.0
 niter = ceil(T/deltat)
-Re = 1600.0
-Pr = 0.72
-mu = 1.0
-Minf = 0.1
-C23 = 2.0/3.0
+#print "LOL", niter
+mu = 1e-1
+k = 0.05
+cv = 718.0
 gamma = 1.4
-simulation_parameters = {"niter":niter, "gama":gamma, "mu":mu,"C23":C23, "Re":Re, "Pr":Pr, "Minf":Minf, "deltat":deltat, "precision":"double", "name":"taylor_green_vortex"}
+simulation_parameters = {"niter":niter, "gama":gamma, "mu":mu, "k":k, "cv":cv, "deltat":deltat, "precision":"double", "name":"taylor_green_vortex"}
 
 # Generate the code.
 opsc = OPSC(grid, spatial_discretisation, temporal_discretisation, boundary, initial_conditions, io, simulation_parameters)
