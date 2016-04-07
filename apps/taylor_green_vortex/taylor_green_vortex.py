460f899e33a8caf4024f835b22177296537cfe24
Christian T. Jacobs <C.T.Jacobs@soton.ac.uk>
2016-04-07 15:12:30 +0100

    Move the TGV_3D into taylor_green_vortex.

--- apps/taylor_green_vortex/taylor_green_vortex.py
+++ apps/taylor_green_vortex/taylor_green_vortex.py
@@ -1,16 +1,17 @@
 #!/usr/bin/env python
 import sys
-from math import ceil
 
 # Import local utility functions
 import opensbli
 from opensbli.problem import *
-from opensbli.latex import LatexWriter
+from opensbli.latex import *
 from opensbli.spatial import *
 from opensbli.bcs import *
 from opensbli.grid import *
 from opensbli.timestepping import *
 from opensbli.io import *
+from opensbli.diagnostics import Reduction
+from opensbli.opsc import OPSC
 
 def dt(dx, c):
     """ Given a grid spacing dx and the wave speed c, return the value of dt such that the CFL condition is respected. """
@@ -19,54 +20,46 @@ def dt(dx, c):
 
 BUILD_DIR = os.getcwd()
 
-opensbli.LOG.info("Generating code for the 3D Taylor-Green vortex simulation...")
+opensbli.LOG.info("Generating code for the 2D Taylor-Green Vortex simulation...")
 start_total = time.time()
-
 # Problem dimension
 ndim = 3
 
 # Define the compresible Navier-Stokes equations in Einstein notation.
-mass = "Eq(Der(rho,t),- Conservative(rhou_j,x_j))"
-momentum = "Eq(Der(rhou_i,t) ,-Conservative(rhou_i*u_j + p* KD(_i,_j),x_j) + Der(tau_i_j,x_j) )"
-energy = "Eq(Der(rhoE,t),- Conservative((p+rhoE)*u_j,x_j) +Der(q_j,x_j) + Der(u_i*tau_i_j ,x_j) )"
+mass = "Eq(Der(rho,t), - Conservative(rhou_j,x_j))"
+momentum = "Eq(Der(rhou_i,t) , -Skew(rhou_i*u_j,x_j) - Der(p,x_i) + Der(tau_i_j,x_j) )"
+energy = "Eq(Der(rhoE,t), - Conservative((p+rhoE)*u_j,x_j) + Der(q_j,x_j) + Der(u_i*tau_i_j ,x_j) )"
+ke = "Eq(ke, rho*(1/2)*u_j*u_j)"
+rhomean = "Eq(rhomean, rho)"
 equations = [mass, momentum, energy]
+diagnostics = [ke, rhomean]
 
 # Substitutions
-stress_tensor = "Eq(tau_i_j, mu*(Der(u_i,x_j)+ Conservative(u_j,x_i)- (2/3)* KD(_i,_j)* Der(u_k,x_k)))"
-heat_flux = "Eq(q_j,  k*Der(T,x_j))"
+stress_tensor = "Eq(tau_i_j, (1.0/Re)*(Der(u_i,x_j)+ Conservative(u_j,x_i)- (2/3)* KD(_i,_j)* Der(u_k,x_k)))"
+heat_flux = "Eq(q_j, (1.0/((gama-1)*Minf*Minf*Pr*Re))*Der(T,x_j))"
 substitutions = [stress_tensor, heat_flux]
 
 # Define all the constants in the equations
-constants = ["gama", "mu", "k", "cv"]
+constants = ["Re", "Pr","gama", "Minf", "C23", "c_j"]
 
-# Coordinate direction symbol (x) this will be x_i, x_j, x_k
+# Define coordinate direction symbol (x) this will be x_i, x_j, x_k
 coordinate_symbol = "x"
 
 # Metrics
-metrics = [False, False, False]
+metrics = [False, False]
 
 # Formulas for the variables used in the equations
 velocity = "Eq(u_i, rhou_i/rho)"
-pressure = "Eq(p, (gama-1)*(rhoE/rho - (1/2)*(u_j*u_j)))"
-temperature = "Eq(T, p/((gama-1)*rho*cv))"
+pressure = "Eq(p, (gama-1)*(rhoE - rho*(1/2)*(u_j*u_j)))"
+temperature = "Eq(T, p*gama*Minf*Minf/(rho))"
 viscosity = "Eq(mu, T**(2/3))"
-formulas = [velocity, pressure, temperature, viscosity]
+formulas = [velocity, pressure, temperature]
 
-# Create the problem and expand the equations.
+# Create the TGV problem and expand the equations.
 problem = Problem(equations, substitutions, ndim, constants, coordinate_symbol, metrics, formulas)
-expanded_equations, expanded_formulas = problem.get_expanded()
-
-# Output equations in LaTeX format.
-latex = LatexWriter()
-latex.open(path=BUILD_DIR + "/equations.tex")
-metadata = {"title": "Equations", "author": "", "institution": ""}
-latex.write_header(metadata)
-temp = flatten(expanded_equations)
-latex.write_expression(temp)
-temp = flatten(expanded_formulas)
-latex.write_expression(temp)
-latex.write_footer()
-latex.close()
+expanded_equations = problem.get_expanded(problem.equations)
+expanded_formulas = problem.get_expanded(problem.formulas)
+expanded_diagnostics = problem.get_expanded(problem.expand(diagnostics))
 
 # Discretise the equations
 start = time.time()
@@ -75,12 +68,12 @@ spatial_scheme = Central(4) # Fourth-order central differencing in space.
 temporal_scheme = RungeKutta(3) # Third-order Runge-Kutta time-stepping scheme.
 
 # Create a numerical grid of solution points
-L = 1
-length = [2*pi*L]*ndim
-np = [25]*ndim
-deltas = [length[i]/np[i] for i in range(len(length))]
-
-grid = Grid(ndim,{'delta':deltas, 'number_of_points':np})
+length = [2.0*pi*1.0]*ndim
+np = [192]*ndim
+deltas = [length[i]/np[i] for i in range(len(length)) ] # how to define them
+print(deltas)
+print(np)
+grid = Grid(ndim,{'delta':deltas, 'number_of_points':np}) # FIXME: A HDF5 file or a user input
 
 # Perform the spatial discretisation
 spatial_discretisation = SpatialDiscretisation(expanded_equations, expanded_formulas, grid, spatial_scheme)
@@ -94,47 +87,50 @@ bcs = [("periodic", "periodic"), ("periodic", "periodic"), ("periodic", "periodi
 boundary = BoundaryConditions(bcs, grid, temporal_discretisation.prognostic_variables)
 
 # Initial conditions
-u0 = 1.0
-p0 = 100.0
-T0 = 300.0
-x = "(grid.Idx[0]*grid.deltas[0])"
-y = "(grid.Idx[1]*grid.deltas[1])"
-z = "(grid.Idx[2]*grid.deltas[2])"
-rhou0 = "(%f*sin(%s)*cos(%s)*cos(%s))" % (u0, x, y, z)
-rhou1 = "(-%f*cos(%s)*sin(%s)*cos(%s))" % (u0, x, y, z)
-p = "%f + (1.0/16.0)*(cos(2*%s) + cos(2*%s))*(cos(2*%s) + 2.0)" % (p0, x, y, z)
-ke = "0.5*(%s*%s + %s*%s)" % (rhou0, rhou0, rhou1, rhou1)
-rhoE = "%s/(1.4-1.0) + %s" % (p, ke)
+x = "Eq(grid.grid_var(x), grid.Idx[0]*grid.deltas[0])"
+y = "Eq(grid.grid_var(y), grid.Idx[1]*grid.deltas[1])"
+z = "Eq(grid.grid_var(z), grid.Idx[2]*grid.deltas[2])"
+u = "Eq(grid.grid_var(u),sin(x)*cos(y)*cos(z))"
+v = "Eq(grid.grid_var(v),-cos(x)*sin(y)*cos(z))"
+w = "Eq(grid.grid_var(w), 0.0)"
+p = "Eq(grid.grid_var(p), 1.0/(gama*Minf*Minf)+ (1.0/16.0) * (cos(2.0*x)+cos(2.0*y))*(2.0 + cos(2.0*z)))"
+r = "Eq(grid.grid_var(r), gama*Minf*Minf*p)"
+initial_conditions = [x,y,z,u,v,w,p,r,
+                      "Eq(grid.work_array(rho), r)",
+                      "Eq(grid.work_array(rhou0), r*u)" ,
+                      "Eq(grid.work_array(rhou1), r*v)",
+                      "Eq(grid.work_array(rhou2), 0.0)",
+                      "Eq(grid.work_array(rhoE), p/(gama-1) + 0.5* r *(u**2+ v**2 + w**2))"]
 
-initial_conditions = ["Eq(grid.work_array(rho), 1.0)",
-                      "Eq(grid.work_array(rhou0), %f*sin(%s)*cos(%s)*cos(%s))" % (u0, x, y, z),
-                      "Eq(grid.work_array(rhou1), -%f*cos(%s)*sin(%s)*cos(%s))" % (u0, x, y, z),
-                      "Eq(grid.work_array(rhou2), 0)",
-                      "Eq(grid.work_array(rhoE), %s)" % rhoE,
-                      "Eq(grid.work_array(p), %f + (1.0/16.0)*(cos(2*%s) + cos(2*%s))*(cos(2*%s) + 2.0))" % (p0, x, y, z),
-                      "Eq(grid.work_array(T), %f)" % (T0)]
 initial_conditions = GridBasedInitialisation(grid, initial_conditions)
+end = time.time()
+LOG.debug('The time taken to prepare the system in %d dimensions is %.2f seconds.' % (problem.ndim, end - start))
 
-# I/O save conservative variables at the end of simulation
-io = FileIO(temporal_discretisation.prognostic_variables)
-
-# Grid parameters like number of points, length in each direction, and delta in each direction
-deltat = 3.385e-3
-T = 5.0#10.0
-niter = ceil(T/deltat)
-#print "LOL", niter
-mu = 1e-1
-k = 0.05
-cv = 718.0
-gamma = 1.4
-simulation_parameters = {"niter":niter, "gama":gamma, "mu":mu, "k":k, "cv":cv, "deltat":deltat, "precision":"double", "name":"taylor_green_vortex"}
-
-# Generate the code.
-opsc = OPSC(grid, spatial_discretisation, temporal_discretisation, boundary, initial_conditions, io, simulation_parameters)
+# Diagnostics
+start = time.time()
+reduction_type = ["sum", "sum", "sum", "sum", "sum"] #  list of reduction types, same length as expanded diagnostics
+red_eq = []
+red_eq.append([Reduction(grid, expanded_diagnostics, expanded_formulas, temporal_discretisation.prognostic_variables, \
+    spatial_scheme, reduction_type, 100)])
 
 
 end = time.time()
-LOG.debug('The time taken to prepare the system in %d dimensions is %.2f seconds.' % (problem.ndim, end - start))
+LOG.debug('The time taken to prepare the reductions in %d dimensions is %.2f seconds.' % (problem.ndim, end - start))
+# I/O save conservative variables at the end of simulation
+io = FileIO(temporal_discretisation.prognostic_variables)
+
+start = time.time()
+# Grid parameters like number of points, length in each direction, and delta in each direction
+l1 = ['niter', 'Re', 'Pr', 'gama', 'Minf', 'mu', 'precision', 'name', 'deltat']
+l2 = [20000, 1600, 0.71, 1.4, 0.1, 1.0, "double", "tgv", 0.001]
+# Constants in the system
+simulation_parameters = dict(zip(l1,l2))
+
+# Generate the code.
+OPSC(grid, spatial_discretisation, temporal_discretisation, boundary, initial_conditions, io, simulation_parameters, red_eq)
+
+end = time.time()
+LOG.debug('The time taken to generate the code in  %d dimensions is %.2f seconds.' % (problem.ndim, end - start))
 
 end_total = time.time()
 LOG.debug('The time taken for the entire process for %d dimensions is %.2f seconds.' % (problem.ndim, end_total - start_total))
