21e15e7f51ed5802df4a54990a5662fa0efacf5a
Christian T. Jacobs <C.T.Jacobs@soton.ac.uk>
2016-04-14 22:07:13 +0100

    Another spatial test for the Central scheme.

--- tests/test_spatial.py
+++ tests/test_spatial.py
@@ -29,24 +29,6 @@ def spatial_derivative(grid, central_scheme, max_order):
     return SpatialDerivative(central_scheme, grid, max_order)
 
 
-@pytest.fixture
-def spatial_discretisation(navier_stokes_problem, grid, central_scheme):
-    expanded_equations = navier_stokes_problem.get_expanded(navier_stokes_problem.equations)
-    expanded_formulas = navier_stokes_problem.get_expanded(navier_stokes_problem.formulas)
-    return SpatialDiscretisation(expanded_equations, expanded_formulas, grid, central_scheme)
-
-
-@pytest.fixture
-def coordinate_symbol():
-    return "x"
-
-
-@pytest.fixture
-def mass(coordinate_symbol):
-    return Equation("Eq(Der(rho,t), -Conservative(rhou_j,x_j))", 2, coordinate_symbol, substitutions=[], constants=[])
-
-
-
 def test_create_stencil(spatial_derivative, central_scheme, grid):
     """ Ensure that a central difference stencil is constructured properly on a given grid. """
 
@@ -56,6 +38,17 @@ def test_create_stencil(spatial_derivative, central_scheme, grid):
         assert len(stencil[dim]) == 5 # 5 point stencil per dimension.
         i = Symbol("i%d" % dim, integer=True)
         assert stencil[dim] == [i-2, i-1, i, i+1, i+2] # Central difference
+        
+    return
+
+
+def test_central_scheme():
+    """ Check that the points of the central scheme are computed correctly for various orders. """
+
+    assert Central(2).points == [-1, 0, 1]
+    assert Central(4).points == [-2, -1, 0, 1, 2]
+    assert Central(6).points == [-3, -2, -1, 0, 1, 2, 3]
+    assert Central(8).points == [-4, -3, -2, -1, 0, 1, 2, 3, 4]
         
     return
 
