82d0fae9f5cd88dbaf3d52849d15bef26422c5aa
Christian T. Jacobs <C.T.Jacobs@soton.ac.uk>
2016-11-13 13:53:28 +0000

    Remove unused sympy import *

--- opensbli/io.py
+++ opensbli/io.py
@@ -18,8 +18,6 @@
 #    You should have received a copy of the GNU General Public License
 #    along with OpenSBLI.  If not, see <http://www.gnu.org/licenses/>.
 
-from sympy import *
-
 
 class FileIO(object):
 
