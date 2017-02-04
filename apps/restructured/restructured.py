d7185a6d72e37410425f252d23976fb2d0d13fad
Satya Jammy <j.satyapramod@gmail.com>
2017-02-04 19:44:46 +0000

    More indentation of WENO Adding weno calling functions to restructured testing script

--- apps/restructured/restructured.py
+++ apps/restructured/restructured.py
@@ -107,7 +107,18 @@ schemes[cent.name] = cent
 # schemes[weno.name] = weno
 rk = RungeKutta(3)
 schemes[rk.name] = rk
-#exit()
+
+
+weno_order = 5
+weno = True
+Euler_eq = EulerEquations(ndim, weno)
+ev_dict, LEV_dict, REV_dict = Euler_eq.generate_eig_system() # Taking more time on my personal computer SPJ
+exit()
+LLF = LLFCharacteristic(ev_dict, LEV_dict, REV_dict, weno_order, ndim)
+schemes[LLF.name] = LLF
+
+# TESTING generate weno scheme
+exit()
 block.sbli_rhs_discretisation = True
 boundaries = [PeriodicBoundaryConditionBlock()]*2*ndim
 block.set_block_boundaries(boundaries)
