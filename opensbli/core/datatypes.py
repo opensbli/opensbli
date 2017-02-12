ea41232dafe2237ef0bcc87e2fabcc43d31a9429
Satya Jammy <j.satyapramod@gmail.com>
2017-02-12 00:05:33 +0000

    Now the datatypes are seperated out these will be used at various places while generating constants/ datasets

---
+++ opensbli/core/datatypes.py
@@ -0,0 +1,23 @@
+
+class SimulationDataType(object):
+    pass
+
+class DataType(object):
+    pass
+
+class Double(DataType):
+    def __init__(self):
+        return
+
+class Float(DataType):
+    def __init__(self):
+        return
+
+class UserDefined(DataType):
+    """ User defined datatype this is either float or double depending on input"""
+    def __init__(self):
+        return
+
+class Int(DataType):
+    def __init__(self):
+        return
