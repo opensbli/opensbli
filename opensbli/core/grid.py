df28e7404613868f7c5c7f2a53712dc3b9098e7a
Satya P. Jammy <s.p.jammy@soton.ac.uk>
2017-06-20 16:56:04 +0100

    More documentation on the work array.

--- opensbli/core/grid.py
+++ opensbli/core/grid.py
@@ -23,43 +23,11 @@ from .opensbliobjects import DataSetBase, GridIndexedBase, ConstantObject
 from sympy.core import Symbol,S, symbols
 from .datatypes import *
 
+class WorkDataSet(object):
+    """ Base object for using work arrays in the OpenSBLI framework. This contains different attributes to control the flow of work arrays in descritisation. This should be used in conjunction with a Simulation block see SimulationBlock .
 
-class GridVariable(Symbol):
+    Instantiated from :class:`.Grid`.
 
-    """ | Defines a new symbolic variable on the Grid. This should be used to create thread or process local variables in a kernel.
-
-        :param str variable: Name of the grid variable required
-        :return: Grid variable
-        :rtype: GridVariable
-
-        **Attributes**: The default attribute assumptions are,
-            | **dtype**: data type of the variable, defaults to None
-            | **is_constant**: weather the variable is a constant or not, defaults to ``False``
-
-        .. note::
-            Grid variable cannot be a constant but used to fit in the abstraction.
-            Later one can use this to define variables in Fortran subroutine.
-
-        >>> a = GridVariable("variable1")
-        >>> srepr(a)
-        GridVariable('variable1')
-        >>> a.__dict__
-        {dtype: None, is_constant: False}
-        """
-
-    def __new__(self, variable):
-        self = Symbol.__new__(self, str(variable))
-        self.is_constant = False
-        self.dtype = None
-        return self
-
-
-class WorkDataSet():
-    """ Base object for using work arrays contains different attributes to control the flow of work arrays
-    in the opensbli framework. This should be used in conjunction with a Simulation block
-    see ``SimulationBlock`` in ``block.py``.
-    Instantiated from Grid
-    Some hard coded dependencies are work array name is always ``wk`` appended by the work index
     """
 
     def __init__(self):
@@ -74,44 +42,52 @@ class WorkDataSet():
     def work_array(self, name = None, location=None):
         """ Sets up a opensbli DataSet, indexed by the relative location.
         By default the range of the data set is that of the the block
-        Parameters
-        ==========
-        :arg str name: The desired name of the work array (optional), defaults to ``wk``
-        :arg location: List of relative indexing for the array (optional), defaults to grid intersection location
+
+        :param str name: The desired name of the work array (optional), defaults to ``None``
+
+            .. note::
+                The work array name defaults to ``wk`` appended by the work index, if no name is provided
+        :param list location: List of relative indexing for the array (optional), defaults to grid intersection location
+
+            .. note::
+                Only default is implemented
         :returns: Opensbli Dataset defined on the grid
-        :rtype: opensbli.DataSet
-        Notes
-        ==========
-        If multiple work arrays are required then the work_index should be incremented
-        before calling this function. If required optionally the user can store the
-        work index in their routine and reset the work index at the end of their routine
-        if all the computations to be performed and the evaluated work arrays are not
-        used any more. This helps in reducing the total memory foot print of the generated
-        code.
-        Example
-        ==========
-        >>> b = SimulationBlock(2, blocknumber=1)
+        :rtype: DataSet
+        :raises ValueError: if location is provided
+
+        .. note::
+            If multiple work arrays are required then the work_index should be incremented
+            before calling this function. If required optionally the user can store the
+            work index in their routine and reset the work index at the end of their routine
+            if all the computations to be performed and the evaluated work arrays are not
+            used any more. This helps in reducing the total memory foot print of the generated
+            code. An example of such usage is,
+
+        >>> b = SimulationBlock(2, blocknumber=1) # Number of dimensions are 2 and block number is 1
         >>> b.work_array()
         wk0_B1[0,0]
-        >>> b.increase_work_index
-        >>> b.work_array()
+        >>> b.increase_work_index # Increases the work array index
+        >>> b.work_index
+        1
+        >>> b.work_array() # Creates a new work array, the index is 1 now
         wk1_B1[0,0]
-        >>> b.reset_work_index
-        >>> b.work_array()
+        >>> b.reset_work_index # Resets the work index
+        >>> b.work_array() # A new work array after reset gives the index as 0
         wk0_B1[0,0]
-        >>> b.increase_work_index
-        >>> b.store_work_index
+        >>> b.increase_work_index # Increase the work array index to 1
+        >>> b.store_work_index # Store the work array index (1)
         >>> b.work_array()
         wk1_B1[0,0]
-        >>> b.increase_work_index
+        >>> b.increase_work_index # Increase the index to 2
         >>> b.work_array()
         wk2_B1[0,0]
-        >>> b.increase_work_index
+        >>> b.increase_work_index # Increase once more to 3
         >>> b.work_array()
         wk3_B1[0,0]
-        >>> b.reset_work_to_stored
-        >>> b.work_array()
+        >>> b.reset_work_to_stored # resets the work array index to 1, previously stored
+        >>> b.work_array() # New work array gives the reset index
         wk1_B1[0,0]
+
         """
         if not name:
             base = self.work_name%self.work_index
@@ -129,26 +105,26 @@ class WorkDataSet():
     @property
     def reset_work_index(self):
         """Resets the work index to zero. Used when we want to re-use work arrays
-        in the discretisation
+        in the discretisation, see the example below for usage
         """
         self.work_index = 0
         return
     @property
     def increase_work_index(self):
         """Increments the work array index by 1. This helps in setting up the next work array
-        index
+        index, see the example below for usage
         """
         self.work_index = self.work_index +1
         return
     @property
     def store_work_index(self):
-        """Stores the current work array index
+        """Stores the current work array index, see the example below for usage
         """
         self.stored_index = self.work_index
         return
     @property
     def reset_work_to_stored(self):
-        """Resets the work array index to the index when the last ``store_work_index`` is called
+        """Resets the work array index to the index when the last ``store_work_index`` is called, see the example below for usage
         """
         self.work_index = self.stored_index
         return
@@ -199,3 +175,33 @@ class Grid(WorkDataSet):
         self.cns_type_code = False # This writes the code similat to traditional compressible NS
         self.sbli_rhs_discretisation = False # this is a pilot implementation of SBLI RHS
         return
+
+
+class GridVariable(Symbol):
+
+    """ | Defines a new symbolic variable on the Grid. This should be used to create thread or process local variables in a kernel.
+
+        :param str variable: Name of the grid variable required
+        :return: Grid variable
+        :rtype: GridVariable
+
+        **Attributes**: The default attribute assumptions are,
+            | **dtype**: data type of the variable, defaults to None
+            | **is_constant**: weather the variable is a constant or not, defaults to ``False``
+
+        .. note::
+            Grid variable cannot be a constant, but used to fit in the abstraction.
+            Later one can use this to define variables in Fortran subroutine.
+
+        >>> a = GridVariable("variable1")
+        >>> srepr(a)
+        GridVariable('variable1')
+        >>> a.__dict__
+        {dtype: None, is_constant: False}
+        """
+
+    def __new__(self, variable):
+        self = Symbol.__new__(self, str(variable))
+        self.is_constant = False
+        self.dtype = None
+        return self
