410388a6f64e8208670ce27551398f067c9a57be
Satya P. Jammy <s.p.jammy@soton.ac.uk>
2017-06-29 10:27:56 +0100

    More optimisations on RA algorithm

--- opensbli/core/optimisations.py
+++ opensbli/core/optimisations.py
@@ -390,3 +390,65 @@ class RA_optimisation_level_2(Central):
             return descritised_equations
         else:
             return None
+
+
+
+
+class RA_optimisation_level_3(Central):
+    """Optimisation this wil work as not classifying the derivatives into
+    convective and viscous terms
+    """
+    def __init__(self, order):
+        Central.__init__(self, order)
+        
+        return
+    
+    def discretise(self, type_of_eq, block):
+        self.set_halos(block) # THis is important
+        pprint(self)
+        equations =  flatten(type_of_eq.equations)
+        residual_arrays = [eq.residual for eq in equations]
+        equations = [e._sanitise_equation for e in equations]
+        rhs_eq = [e.rhs for e in equations]
+        #print "nhere"
+        self.required_constituent_relations = {}
+        equations = [Eq(x,y) for x,y in zip(residual_arrays, rhs_eq)]
+        discretised_eq = self.RA(equations, block)
+        if discretised_eq:
+            for no, eq in enumerate(discretised_eq):
+                discretisation_kernel = Kernel(block, computation_name="%s evaluation%d"%(type_of_eq.__class__.__name__, no))
+                discretisation_kernel.set_grid_range(block)
+                discretisation_kernel.add_equation(eq)
+                discretisation_kernel.update_block_datasets(block)
+                type_of_eq.Kernels +=  [discretisation_kernel]
+        return self.required_constituent_relations
+    
+    def RA(self, equations, block):
+        cds = self.get_local_function(equations)
+        descritised_equations = equations[:]
+        #pprint(cds)
+        work_arry_subs = {}
+        if cds:
+            for der in sorted(cds, cmp=decreasing_order):
+                self.update_range_of_constituent_relations(der, block)
+                expr = der.copy()
+                inner_cds = []
+                #if CD.args[0].atoms(CentralDerivative):
+                pot = postorder_traversal(expr)
+                inner_cds = []
+                for p in pot:
+                    if isinstance(p, CentralDerivative):
+                        inner_cds += [p]
+                    else:
+                        continue
+                # Contains inner derivatives
+                if len(inner_cds)>1:
+                    for np,cd in enumerate(inner_cds[:-1]):
+                        expr = expr.subs(cd, cd._discretise_derivative(self, block))
+                expr_discretised = expr._discretise_derivative(self, block)
+                
+                for no, c in enumerate(descritised_equations):
+                    descritised_equations[no] = descritised_equations[no].subs(der, expr_discretised)
+            return descritised_equations
+        else:
+            return None
