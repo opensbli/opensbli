82e103d0bfba32d6e716fcecede6605ff673ae79
Satya P Jammy <j.satyapramod@gmail.com>
2017-08-31 14:09:56 +0100

    Flake 8 issues

--- opensbli/core/datatypes.py
+++ opensbli/core/datatypes.py
@@ -3,26 +3,33 @@ class SimulationDataType(object):
     @staticmethod
     def set_datatype(types):
         SimulationDataType.dtype = types
+
     @staticmethod
     def opsc():
         return SimulationDataType.dtype.opsc()
 
+
 class DataType(object):
     pass
+
 
 class Double(DataType):
     @staticmethod
     def opsc():
         return "double"
 
+
 class Float(DataType):
     def __init__(self):
         return
 
+
 class UserDefined(DataType):
     """ User defined datatype this is either float or double depending on input"""
+
     def __init__(self):
         return
+
 
 class Int(DataType):
     @staticmethod
