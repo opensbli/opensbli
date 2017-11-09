13a7550e7787e9718c769f6f65a4c62f46732a42
Satya P Jammy <j.satyapramod@gmail.com>
2017-11-09 11:06:09 +0000

    Bumped the version number and changed development status

--- setup.py
+++ setup.py
@@ -1,34 +1,17 @@
 #!/usr/bin/env python
 
-#    OpenSBLI: An automatic code generator for solving differential equations.
-#    Copyright (C) 2016 Satya P. Jammy, Christian T. Jacobs, Neil D. Sandham
-
-#    This file is part of OpenSBLI.
-
-#    OpenSBLI is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU General Public License as published by
-#    the Free Software Foundation, either version 3 of the License, or
-#    (at your option) any later version.
-
-#    OpenSBLI is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU General Public License for more details.
-
-#    You should have received a copy of the GNU General Public License
-#    along with OpenSBLI.  If not, see <http://www.gnu.org/licenses/>.
-
 from distutils.core import setup
 
 setup(name='OpenSBLI',
-      version='1.0',
+      version='2.b0',
       description='An automatic code generator which expands a set of equations written in Einstein notation, and writes out the finite difference code in a supported language.',
-      author='Satya P. Jammy, Christian T. Jacobs, Neil D. Sandham',
+      author='',
       url='https://bitbucket.org/spjammy/opensbli',
       packages=['opensbli'],
       package_dir={'opensbli': 'opensbli'},
       scripts=[],
       classifiers=[
+      'Development Status :: 2 - Beta',
       'Environment :: Console',
       'Intended Audience :: Science/Research',
       'Natural Language :: English',
