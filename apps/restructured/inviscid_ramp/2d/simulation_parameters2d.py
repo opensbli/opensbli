87a041a2745b65112feae9bc4ae735a97e8f54d8
Satya P. Jammy <j.satyapramod@gmail.com>
2017-11-15 18:51:11 +0000

    Single file to generate multiple order codes for weno and teno

--- apps/restructured/inviscid_ramp/2d/simulation_parameters2d.py
+++ apps/restructured/inviscid_ramp/2d/simulation_parameters2d.py
@@ -14,8 +14,8 @@ def substitute_parameters(simulation_name, constants, values):
         f.write(s)
     return
 
-if __name__ == "__main__":
-    constants = ['gama', 'Minf', 'dt', 'niter', 'block0np0', 'block0np1', 'Delta0block0', 'Delta1block0', 'lx', 'ly']
-    values = ['1.4', '2.0', '0.005', '1000', '100', '100', '3.0/(block0np0-1)', '1.0/(block0np1-1)', "3.0", "1.0"]
-    simulation_name = 'opensbli'
-    substitute_parameters(simulation_name, constants, values)
+#if __name__ == "__main__":
+    #constants = ['gama', 'Minf', 'dt', 'niter', 'block0np0', 'block0np1', 'Delta0block0', 'Delta1block0', 'lx', 'ly']
+    #values = ['1.4', '2.0', '0.005', '1000', '100', '100', '3.0/(block0np0-1)', '1.0/(block0np1-1)', "3.0", "1.0"]
+    #simulation_name = 'opensbli'
+    #substitute_parameters(simulation_name, constants, values)
