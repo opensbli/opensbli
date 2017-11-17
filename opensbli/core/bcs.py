2a8d5804ea49dcef74d32ea44d038d91c28fde4e
Satya P Jammy <j.satyapramod@gmail.com>
2017-11-17 13:06:02 +0000

    Fixes to the symmetry Boundary condition

--- opensbli/core/bcs.py
+++ opensbli/core/bcs.py
@@ -349,7 +349,7 @@ class DirichletBoundaryConditionBlock(ModifyCentralDerivative, BoundaryCondition
         return kernel
 
 
-class SymmetryBoundaryConditionBlock(BoundaryConditionBase):
+class SymmetryBoundaryConditionBlock(ModifyCentralDerivative, BoundaryConditionBase):
     """ Applies a symmetry condition on the boundary, normal velocity components set/evaluate to zero.
 
     :arg int boundary_direction: Spatial direction to apply boundary condition to.
@@ -374,19 +374,26 @@ class SymmetryBoundaryConditionBlock(BoundaryConditionBase):
         normalisation = sqrt(sum([a**2 for a in direction_metric]))
         unit_normals = direction_metric/normalisation
         lhs_eqns = flatten(arrays)
+        boundary_values = []
         rhs_eqns = []
         for ar in arrays:
             if isinstance(ar, list):
                 contra_variant_vector = unit_normals.dot(Matrix(ar))
                 transformed_vector = Matrix(ar).T - 2.*contra_variant_vector*unit_normals
                 rhs_eqns += flatten(transformed_vector)
+                # Later move this to an inviscid wall boundary condition
+                transformed_vector = Matrix(ar).T - contra_variant_vector*unit_normals
+                boundary_values += flatten(transformed_vector)
             else:
                 rhs_eqns += [ar]
+                boundary_values += [ar]
         halos, kernel = self.generate_boundary_kernel(block, self.bc_name)
         from_side_factor, to_side_factor = self.set_side_factor()
 
         transfer_indices = [tuple([from_side_factor*t, to_side_factor*t]) for t in range(1, abs(halos[direction][side]) + 1)]
         final_equations = self.create_boundary_equations(lhs_eqns, rhs_eqns, transfer_indices)
+        transfer_indices = [tuple([0, 1])]
+        final_equations += self.create_boundary_equations(lhs_eqns, boundary_values, transfer_indices)
         kernel.add_equation(final_equations)
         kernel.update_block_datasets(block)
         return kernel
