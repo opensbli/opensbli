3d19b6c362ac08a0806fa7783b4c1759732c5bf8
David <dl11g11@soton.ac.uk>
2017-11-28 13:58:00 +0000

    Added Forcing strip BC for transitional case

--- opensbli/core/bcs.py
+++ opensbli/core/bcs.py
@@ -127,6 +127,7 @@ class BoundaryConditionBase(object):
         self.equations = None
         return
 
+
     def convert_dataobject_to_dataset(self, block):
         """ Converts DataObjects to DataSets.
         :arg object block: OpenSBLI SimulationBlock."""
@@ -830,6 +831,77 @@ class AdiabaticWallBoundaryConditionBlock(ModifyCentralDerivative, BoundaryCondi
                     rhs_eqns += [ar]
             transfer_indices = [tuple([from_side_factor*t, to_side_factor*t]) for t in range(1, abs(halos[self.direction][self.side]) + 1)]
             final_equations = self.create_boundary_equations(lhs_eqns, rhs_eqns, transfer_indices)
+        kernel.add_equation(final_equations)
+        kernel.update_block_datasets(block)
+        return kernel
+
+class ForcingStripWallBoundaryConditionBlock(ModifyCentralDerivative, BoundaryConditionBase):
+    """ Navier-Stokes specific boundary condition. Applies a no-slip viscous wall condition, 
+    velocity components are zero on the wall. Temperature is fixed with a prescribed wall temperature, 
+    given as the rhoE equation passed to this BC. A wall normal velocity is set based on the equation passed by the user.
+
+    :arg int boundary_direction: Spatial direction to apply boundary condition to.
+    :arg int side: Side 0 or 1 to apply the boundary condition for a given direction.
+    :arg Eq equations: Equation for conservative variable rhoE to set on the wall with constant temperature.
+    :arg object scheme: Boundary scheme if required, defaults to Carpenter boundary treatment.
+    :arg bool plane: True/False: Apply boundary condition to full range/split range only."""
+    def __init__(self, boundary_direction, side, wall_normal_velocity, equations, scheme=None, plane=True):
+        BoundaryConditionBase.__init__(self, boundary_direction, side, plane)
+        self.bc_name = 'ForcingStripWall'
+        self.equations = equations
+        self.wall_normal_velocity = wall_normal_velocity
+        if not scheme:
+            self.modification_scheme = Carpenter()
+        else:
+            self.modification_scheme = scheme
+        return
+
+    def apply(self, arrays, block):
+        halos, kernel = self.generate_boundary_kernel(block, self.bc_name)
+        n_halos = abs(halos[self.direction][self.side])
+        # Using Navier Stokes physics object, create conservative variables
+        NS = NSphysics(block)
+        cons_vars = [NS.density(), NS.momentum(), NS.total_energy()]
+        base_loc = list(cons_vars[0].indices)
+        # Set wall conditions, momentum zero, rhoE specified:
+        wall_momentum = [Eq(x, Float(S.Zero)) for x in NS.momentum()]
+        del wall_momentum[self.direction]
+        wall_eqns = block.dataobjects_to_datasets_on_block([self.wall_normal_velocity])
+        wall_eqns += wall_momentum + self.equations[:]
+        kernel.add_equation(wall_eqns)
+
+        # Update halos if a shock capturing scheme is being used.
+        final_equations = []
+        if any(isinstance(sc, ShockCapturing) for sc in block.discretisation_schemes.values()):
+            # Evaluate the wall pressure
+            p0, gama, Minf = GridVariable('p0'), NS.specific_heat_ratio(), NS.mach_number()
+            kernel.add_equation(Eq(p0, NS.pressure(relation=True, conservative=True)))
+            # Temperature evaluations for the halos
+            from_side_factor, to_side_factor = self.set_side_factor()
+            for i in range(1, n_halos+1):
+                new_loc = base_loc[:]
+                new_loc[self.direction] += to_side_factor*i
+                T = self.convert_dataset_base_expr_to_datasets(NS.temperature(relation=True, conservative=True), new_loc)
+                kernel.add_equation(Eq(GridVariable('T%d' % i), T))
+
+            # Set rhoE RHS and reverse momentum components in the halos
+            rhs_eqns = flatten([0, [-1*x for x in NS.momentum()], Float(S.Zero)])
+            rhs_eqns[-1] = p0/(gama-1.0) + 0.5*dot(NS.momentum(), NS.momentum())/NS.density()
+            # Transfer indices are the indices of halo points
+            transfer_indices = [tuple([from_side_factor*t, to_side_factor*t]) for t in range(1, n_halos + 1)]
+
+            for i, index in enumerate(transfer_indices):
+                array_equations = []
+                loc_lhs, loc_rhs = base_loc[:], base_loc[:]
+                loc_lhs[self.direction] += index[0]
+                loc_rhs[self.direction] += index[1]
+                # Set rho RHS
+                rhs_eqns[0] = p0*gama*Minf**2 / GridVariable('T%d' % (i+1))
+                for left, right in zip(flatten(cons_vars), rhs_eqns):
+                    left = self.convert_dataset_base_expr_to_datasets(left, loc_lhs)
+                    right = self.convert_dataset_base_expr_to_datasets(right, loc_rhs)
+                    array_equations += [Eq(left, right, evaluate=False)]
+                final_equations += array_equations
         kernel.add_equation(final_equations)
         kernel.update_block_datasets(block)
         return kernel
