e357f53fba27c712e70f0ab638c32d035bdcd127
Satya P Jammy <j.satyapramod@gmail.com>
2018-02-17 19:50:52 +0000

    Preparing turbulent channel flow case for the v2.0 paper

--- apps/restructured/channel_flow/channel_flow_turbulent_3d/channel_flow_wall.py
+++ apps/restructured/channel_flow/channel_flow_turbulent_3d/channel_flow_wall.py
@@ -4,14 +4,15 @@ from opensbli import *
 import copy
 from opensbli.utilities.helperfunctions import substitute_simulation_parameters
 
+# STEP 0 Create the equations required for the numerical solution
 # Problem dimension
 ndim = 3
 
-# Define the compresible Navier-Stokes equations in Einstein notation, by default the scheme is Central no need to
-# Specify the schemes
+# Define the compresible Navier-Stokes equations in Einstein notation
+
 mass = "Eq(Der(rho,t), - Skew(rho*u_j,x_j))"
-momentum = "Eq(Der(rhou_i,t) , - Skew(rhou_i*u_j, x_j) - Der(p,x_i)  + Der(tau_i_j,x_j))"
-energy = "Eq(Der(rhoE,t), - Skew(rhoE*u_j,x_j) - Conservative(p*u_j,x_j) + Der(q_j,x_j) + Der(u_i*tau_i_j ,x_j))"
+momentum = "Eq(Der(rhou_i,t) , - Skew(rhou_i*u_j, x_j) - Der(p,x_i)  + Der(tau_i_j,x_j) - KD(_i,_j)*c_j))"
+energy = "Eq(Der(rhoE,t), - Skew(rhoE*u_j,x_j) - Conservative(p*u_j,x_j) - Dot(c_j, u_j) + Der(q_j,x_j) + Der(u_i*tau_i_j ,x_j))"
 
 # Substitutions used in the equations
 stress_tensor = "Eq(tau_i_j, (1.0/Re)*(Der(u_i,x_j)+ Der(u_j,x_i)- (2/3)* KD(_i,_j)* Der(u_k,x_k)))"
@@ -20,7 +21,7 @@ heat_flux = "Eq(q_j, (1.0/((gama-1)*Minf*Minf*Pr*Re))*Der(T,x_j))"
 substitutions = [stress_tensor, heat_flux]
 
 # Constants that are used
-constants = ["Re", "Pr", "gama", "Minf", "mu"]
+constants = ["Re", "Pr", "gama", "Minf", "c_j"]
 
 # symbol for the coordinate system in the equations 
 coordinate_symbol = "x"
@@ -79,82 +80,40 @@ latex.close()
 # Create a simulation block
 block = SimulationBlock(ndim, block_number=0)
 
-# Local dictionary for parsing the expressions
-local_dict = {"block": block, "GridVariable": GridVariable, "DataObject": DataObject}
+# Define the variables used for creating boundary conditions and the initialisation
+# dx and dy of the grid
+dx, dy, dz = block.deltas
+# Indices for the grid location
+i,j,k = block.grid_indexes
+# Some constants used 
+gama, Minf, Re = symbols('gama Minf Re', **{'cls': ConstantObject})
+""" Conservative vector is the time advancement arrays of the simulation equations.
+the order follows the order in which they are added to the simulation equations
+class, i.e. arrays of density, momentum (components), energy in the present case 
+"""
+q_vector = flatten(simulation_eq.time_advance_arrays)
 
-# Initial conditions as strings
-x0 = "Eq(DataObject(x0), block.deltas[0]*block.grid_indexes[0])"
-x1 = "Eq(DataObject(x1), block.deltas[1]*block.grid_indexes[1])"
-x2 = "Eq(DataObject(x2), block.deltas[2]*block.grid_indexes[2])"
-
-# Turbulent initial condition
-# xl = 2.0*pi
-# yl = 2.0
-# zl = pi
-x0l = "Eq(GridVariable(x0l), 2.0*pi)"
-x1l = "Eq(GridVariable(x1l), 2.0)"
-x2l = "Eq(GridVariable(x2l), pi)"
-
-sx0 = "Eq(GridVariable(sx0), sin(4.0*pi*DataObject(x0)/x0l) )"
-sx1 = "Eq(GridVariable(sx1), sin(pi*(DataObject(x1)-1.0)) )"
-sx2 = "Eq(GridVariable(sx2), sin(2.0*pi*DataObject(x2)/x2l) )"
-cx0 = "Eq(GridVariable(cx0), cos(4.0*pi*DataObject(x0)/x0l) )"
-cx1 = "Eq(GridVariable(cx1), 1.0+cos(pi*(DataObject(x1)-1.0)) )"
-cx2 = "Eq(GridVariable(cx2), cos(2.0*pi*DataObject(x2)/x2l) )"
-x1wall = "Eq(GridVariable(x1wall), Abs(1.0 - Abs(DataObject(x1)-1.0)) )"
-
-
-vonkar = "Eq(GridVariable(vonkar), 2.5)"
-b = "Eq(GridVariable(b), 5.5)"
-visc = "Eq(GridVariable(visc), 1.0/180.0)"
-amp = "Eq(GridVariable(amp), 0.1*(vonkar*log(1.0/visc)+b))"
-
-ubar = "Eq(GridVariable(ubar), Piecewise((x1wall/visc, x1wall/visc < 10.0), (vonkar*log(x1wall/visc)+b, True)) )"
-
-u0 = "Eq(GridVariable(u0), ubar+amp*x0l/2.0*cx0*sx2*sx1)"
-u1 = "Eq(GridVariable(u1), -amp*sx0*sx2*cx1)"
-u2 = "Eq(GridVariable(u2), -amp*(x2l/2.0)*sx0*cx2*sx1)"
-p = "Eq(GridVariable(p), 1.0/(gama*Minf*Minf))"
-r = "Eq(GridVariable(r), gama*Minf*Minf*p)"
-
-rho = "Eq(DataObject(rho), r)"
-rhou0 = "Eq(DataObject(rhou0), r*u0)"
-rhou1 = "Eq(DataObject(rhou1), r*u1)"
-rhou2 = "Eq(DataObject(rhou2), r*u2)"
-rhoE = "Eq(DataObject(rhoE), p/(gama-1) + 0.5* r*(u0**2 + u1**2 + u2**2))"
-eqns = [x0, x1, x2, x0l, x1l, x2l, sx0, sx1, sx2, cx0, cx1, cx2, x1wall, vonkar, b, visc, amp, ubar, u0, u1, u2, p, r, rho, rhou0, rhou1, rhou2, rhoE]
-
-# parse the initial conditions
-initial_equations = [parse_expr(eq, local_dict=local_dict) for eq in eqns]
-pprint(initial_equations)
-initial = GridBasedInitialisation()
-initial.add_equations(initial_equations)
-
-
-# Create a schemes dictionary to be used for discretisation
-schemes = {}
-# Central scheme for spatial discretisation and add to the schemes dictionary
-cent = Central(4)
-schemes[cent.name] = cent
-# RungeKutta scheme for temporal discretisation and add to the schemes dictionary
-rk = RungeKutta(3)
-schemes[rk.name] = rk
-
-
+# STEP 1
+# Set the boundary conditions on the block
 boundaries = []
+# For laminar channel flow case the boundaries are periodic in x and walls in y
 # Periodic boundaries in x0 direction
 direction = 0
-boundaries += [PeriodicBC(direction, 0)]
-boundaries += [PeriodicBC(direction, 1)]
+boundaries += [PeriodicBC(direction, side=0)]
+boundaries += [PeriodicBC(direction, side=1)]
 
 # Isothermal wall in x1 direction
+# Energy on the wall is set 
+wall_energy = [Eq(q_vector[4], q_vector[0]/(gama * Minf**2.0 * (gama - S.One)))]
+
 direction = 1
-rhoEd = "Eq(DataObject(rhoE), DataObject(rho)/((gama-1)*gama*Minf*Minf))"
-rhoEd = parse_expr(rhoEd, local_dict=local_dict)
-upper_wall_eq = [rhoEd]
-lower_wall_eq = [rhoEd]
-boundaries += [IsothermalWallBC(direction, 0, upper_wall_eq)]
-boundaries += [IsothermalWallBC(direction, 1, lower_wall_eq)]
+# Side 0 (bottom wall) boundary
+lower_wall_eq = wall_energy[:]
+boundaries += [IsothermalWallBC(direction, 0, lower_wall_eq)]
+
+# Side 1 (top) boundary
+upper_wall_eq = wall_energy[:]
+boundaries += [IsothermalWallBC(direction, 1, upper_wall_eq)]
 
 # Periodic boundaries in x2 direction
 direction = 2
@@ -163,22 +122,95 @@ boundaries += [PeriodicBC(direction, 1)]
 
 # set the boundaries for the block
 block.set_block_boundaries(boundaries)
-# set the equations to be solved on the block
-block.set_equations([copy.deepcopy(constituent), copy.deepcopy(simulation_eq), initial])
-# set the discretisation schemes 
+
+# Create the grid and intial conditions
+# Arrays to store x and y coordinates, i.e (x0 and x1)
+x,y,z = symbols('x0:%d' %ndim, **{'cls':DataObject})
+grid_equations = []
+# Equations for generating the grid, simple equispacing grid, later change to stretched
+grid_equations += [Eq(x, i*dx), Eq(y, -S.One + j*dy), Eq(z, k*dz)]
+
+# Initial condition
+initial_equations = []
+
+lx, ly, lz = symbols('lx0:%s' % ndim, **{'cls': ConstantObject})
+sx, sy, sz = symbols('sx0:%s' % ndim, **{'cls': GridVariable})
+cx, cy, cz = symbols('cx0:%s' % ndim, **{'cls': GridVariable})
+ubar, amp, vonkar, b = symbols('ubar amp vonkar b', **{'cls': GridVariable})
+
+# Set the constants for initialisation
+initial_equations += [Eq(b, 5.5), Eq(vonkar, 2.5)]
+
+# Equation for umean
+initial_equations += [Eq(ubar, Piecewise((y*Re, y*Re < 10.0), (vonkar*log(y*Re) + b, True)))]
+# Amplitude of the disturbances
+initial_equations += [Eq(amp, 0.1 * (vonkar*log(Re) + b))]
+
+# sin disturbances in each direction
+initial_equations += [Eq(sx, sin(4.0*pi*x/lx)), Eq(sy, sin(pi*y)), Eq(sz, sin(2.0*pi*z/lz))]
+
+# cosine disturbances in each direction
+initial_equations += [Eq(cx, cos(4.0*pi*x/lx)), Eq(cy, S.One + cos(pi*y)), Eq(cz, cos(2.0*pi*z/lz))]
+
+d, u, v, w, p = symbols('d u0 u1 u2 p', **{'cls': GridVariable})
+# Find the turbule u, v, w 
+initial_equations += [Eq(u, ubar + amp*(lx/2.0)*cx*sy*sz)]
+initial_equations += [Eq(v, - amp*sx*cy*sz)]
+initial_equations += [Eq(w, - amp*(lz/2.0)*sx*sy*cz)]
+initial_equations += [Eq(p, S.One/(gama*Minf**2.0))]
+initial_equations += [Eq(d, S.One)]
+
+# Now evaluate the Conservative vector
+
+initial_equations += [Eq(q_vector[0], d)]
+initial_equations += [Eq(q_vector[1], d*u), Eq(q_vector[2], d*v), Eq(q_vector[3], d*w)]
+initial_equations += [Eq(q_vector[4], p/(gama - S.One) + 0.5 *d(u**2 + v**2 + w**2))]
+
+# Instantiate a grid based initialisation classes
+initial = GridBasedInitialisation()
+initial.add_equations(grid_equations + initial_equations)
+
+# STEP 2
+# Set the equation classes for the block (list)
+block.set_equations([constituent, simulation_eq, initial])
+
+# STEP 3
+# Create the dictionary of schemes
+schemes = {}
+# Central scheme for spatial discretisation and add to the schemes dictionary
+cent = Central(4)
+schemes[cent.name] = cent
+# RungeKutta scheme for temporal discretisation and add to the schemes dictionary
+rk = RungeKutta(3)
+schemes[rk.name] = rk
+# Set the discretisation schemes to be used (a python dictionary)
 block.set_discretisation_schemes(schemes)
 
-# Discretise the equations on the block
+# STEP 4 add io for the block
+kwargs = {'iotype': "Write"}
+output_arrays = simulation_eq.time_advance_arrays + [x,y,z]
+output_hdf5 = iohdf5(arrays=output_arrays, **kwargs)
+block.setio([output_hdf5])
+
+# STEP 6
+# Perform the symbolic discretisation of the equations 
 block.discretise()
 
-# create an algorithm from the discretised computations, this writes the file algorithm.tex in latex_output directory
+# STEP 7
+# create an algorithm from the numerical solution
 alg = TraditionalAlgorithmRK(block)
 
-# set the simulation data type, for more information on the datatypes see opensbli.core.datatypes
+# STEP 8 
+# set the simulation data type: if not set "Double" is default
 SimulationDataType.set_datatype(Double)
 
-# Write the code for the algorithm
+# STEP 9
+# Write the OPSC compatible code for the numerical solution
 OPSC(alg)
+
+# STEP 10 
+# Populate the values of the constants like Re, Pr etc and the number of points for the
+# simulation etc. In the future reading thes from HDF5 would be provided
 constants = ['Re', 'gama', 'Minf', 'Pr', 'dt', 'niter', 'block0np0', 'block0np1', 'block0np2', 'Delta0block0', 'Delta1block0', 'Delta2block0', "c0", "c1", "c2"]
 values = ['180.0', '1.4', '0.01', '0.72', '0.00001', '100000', '64', '256', '64', '2.0*M_PI/block0np0', '2.0/(block0np1-1)', 'M_PI/block0np2', '-1', '0', '0']
 substitute_simulation_parameters(constants, values)
