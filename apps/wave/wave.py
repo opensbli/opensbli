1e69b58fdf7e58e069c56faac93419585daa5410
Satya P Jammy <j.satyapramod@gmail.com>
2018-08-30 09:35:10 +0100

    Added licence to all the applications

--- apps/wave/wave.py
+++ apps/wave/wave.py
@@ -1,4 +1,23 @@
 #!/usr/bin/env python
+
+#    OpenSBLI: An automatic code generator for solving differential equations.
+#    Copyright (c) see License file
+
+#    This file is part of OpenSBLI.
+
+#    OpenSBLI is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+
+#    OpenSBLI is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+
+#    You should have received a copy of the GNU General Public License
+#    along with OpenSBLI.  If not, see <http://www.gnu.org/licenses/>.
+
 # Import all the functions from opensbli
 from opensbli import *
 import copy
