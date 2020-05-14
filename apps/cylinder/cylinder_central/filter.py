13a4c56387e5da35a3e743acc31c769b59e72f2b
David Lusher <dl11g11@soton.ac.uk>
2020-05-14 23:03:03 +0100

    Cylinder updates

--- apps/cylinder/cylinder_central/filter.py
+++ apps/cylinder/cylinder_central/filter.py
@@ -1,8 +1,8 @@
 """Three point filter. Original version by Alex Gillespie."""
 
 from opensbli import *
-from sympy import symbols, exp, pprint
-from opensbli.core.opensbliobjects import DataObject, ConstantObject
+from sympy import symbols, exp, pprint, Piecewise
+from opensbli.core.opensbliobjects import DataObject, ConstantObject, GroupedPiecewise
 from opensbli.equation_types.opensbliequations import OpenSBLIEquation
 from opensbli.postprocess.post_process_eq import *
 from opensbli.core.kernel import ConstantsToDeclare as CTD
@@ -10,77 +10,94 @@ from opensbli.code_generation.algorithm.common import *
 from opensbli.utilities.user_defined_kernels import UserDefinedEquations
 
 class Filter(object):
-	def __init__(self, block, sigma=0.05):
-		self.block = block
-		sigma_symbol = ConstantObject('sigma_filt')
-		sigma_symbol.value = sigma
-		self.sigma = sigma_symbol
-		self.equation_classes = []
-		self.weights = [0.25, 0.5, 0.25]
-		# Create the filter equations
-		self.create_filter()
-		return
+    def __init__(self, block, grid_condition=None, sigma=0.05):
+        self.block = block
+        # Spatial dependence of the filter
+        self.grid_condition = grid_condition
+        sigma_symbol = ConstantObject('sigma_filt')
+        sigma_symbol.value = sigma
+        self.sigma = sigma_symbol
+        self.equation_classes = []
+        self.weights = [0.25, 0.5, 0.25]
+        # Create the filter equations
+        self.create_filter()
+        return
 
-	def create_stencil(self, block, q, direction):
-		output = []
-		for dset in q:
-			dset = block.location_dataset(dset)
-			dset = [increment_dataset(dset, direction, -1), dset, increment_dataset(dset, direction, 1)]
-			output += [dset]
-		return output
+    def create_stencil(self, block, q, direction):
+        output = []
+        for dset in q:
+            dset = block.location_dataset(dset)
+            dset = [increment_dataset(dset, direction, -1), dset, increment_dataset(dset, direction, 1)]
+            output += [dset]
+        return output
 
-	def filtered_equations(self, q_f, q_stencil):
-		a, b, c = self.weights
-		output = []
-		for u_f, u_stencil in zip(q_f, q_stencil):
-			output += [OpenSBLIEquation(u_f, a*u_stencil[0] + b*u_stencil[1] + c*u_stencil[2])]
-		return output
+    def filtered_equations(self, q_f, q_stencil):
+        a, b, c = self.weights
+        output = []
+        for u_f, u_stencil in zip(q_f, q_stencil):
+            output += [OpenSBLIEquation(u_f, a*u_stencil[0] + b*u_stencil[1] + c*u_stencil[2])]
+        return output
+
+    def conditional_expression(self, filter_equations):
+        """ Only applies the filter in certain regions of the domain. Based on the 
+        grid_condition input to the class, which should be a boolean expression built from the coordinate arrays."""
+        output_equations = []
+        for i, eqn in enumerate(filter_equations):
+            condition = ExprCondPair(eqn.rhs, self.grid_condition)
+            output_equations += [OpenSBLIEquation(eqn.lhs, Piecewise(condition, (eqn.lhs, True)))]
+        return output_equations
 
 
-	def create_equations(self):
-		block = self.block
-		ndim = block.ndim
-		# Conservative variables
-		if ndim == 2:
-			q = ['rho', 'rhou0', 'rhou1', 'rhoE']
-		elif ndim == 3:
-			q = ['rho', 'rhou0', 'rhou1', 'rhou2', 'rhoE']
-		# q = [DataObject('%s' % var) for var in cons_vars]
+    def create_equations(self):
+        block = self.block
+        ndim = block.ndim
+        # Conservative variables
+        if ndim == 2:
+            q = ['rho', 'rhou0', 'rhou1', 'rhoE']
+        elif ndim == 3:
+            q = ['rho', 'rhou0', 'rhou1', 'rhou2', 'rhoE']
+        # q = [DataObject('%s' % var) for var in cons_vars]
 
-		# Create the three point stencils
-		q_xstencil = self.create_stencil(block, q, 0)
-		q_ystencil = self.create_stencil(block, q, 1)
-		q_fx = [GridVariable(u + "_xfiltered") for u in q]
-		q_fy = [GridVariable(u + "_yfiltered") for u in q]
-		if ndim == 3:
-			q_zstencil = self.create_stencil(block, q, 2)
-			q_fz = [GridVariable(u + "_zfiltered") for u in q]
-		
-		# Create the filter equations
-		output_equations = self.filtered_equations(q_fx, q_xstencil)
-		output_equations += self.filtered_equations(q_fy, q_ystencil)
-		if ndim == 3:
-			output_equations += self.filtered_equations(q_fz, q_zstencil)
+        # Create the three point stencils
+        q_xstencil = self.create_stencil(block, q, 0)
+        q_ystencil = self.create_stencil(block, q, 1)
+        q_fx = [GridVariable(u + "_xfiltered") for u in q]
+        q_fy = [GridVariable(u + "_yfiltered") for u in q]
+        if ndim == 3:
+            q_zstencil = self.create_stencil(block, q, 2)
+            q_fz = [GridVariable(u + "_zfiltered") for u in q]
+        
+        # Create the filter equations
+        output_equations = self.filtered_equations(q_fx, q_xstencil)
+        output_equations += self.filtered_equations(q_fy, q_ystencil)
+        if ndim == 3:
+            output_equations += self.filtered_equations(q_fz, q_zstencil)
 
-		# Average the filter
-		q_f = [GridVariable(u + "_filtered") for u in q]
-		if ndim == 2:
-			for u_f, u_fx, u_fy in zip(q_f, q_fx, q_fy):
-				output_equations += [OpenSBLIEquation(u_f, (u_fx + u_fy)/2.0)]
-		elif ndim == 3:
-			for u_f, u_fx, u_fy, u_fz in zip(q_f, q_fx, q_fy, q_fz):
-				output_equations += [OpenSBLIEquation(u_f, (u_fx + u_fy + u_fz)/3.0)]
-		# Blend the filter
-		for u, u_f in zip(q, q_f):
-			u = block.location_dataset(u)
-			output_equations += [OpenSBLIEquation(u, (1-self.sigma)*u + self.sigma*u_f)]
-		return output_equations
+        # Average the filter
+        q_f = [GridVariable(u + "_filtered") for u in q]
+        if ndim == 2:
+            for u_f, u_fx, u_fy in zip(q_f, q_fx, q_fy):
+                output_equations += [OpenSBLIEquation(u_f, (u_fx + u_fy)/2.0)]
+        elif ndim == 3:
+            for u_f, u_fx, u_fy, u_fz in zip(q_f, q_fx, q_fy, q_fz):
+                output_equations += [OpenSBLIEquation(u_f, (u_fx + u_fy + u_fz)/3.0)]
+        # Blend the filter
+        blended_equations = []
+        for u, u_f in zip(q, q_f):
+            u = block.location_dataset(u)
+            blended_equations += [OpenSBLIEquation(u, (1-self.sigma)*u + self.sigma*u_f)]
+        # Apply the spatial condition
+        if self.grid_condition is not None:
+            output_equations += self.conditional_expression(blended_equations)
+        else:
+            output_equations += blended_equations
+        return output_equations
 
-	def create_filter(self):
-		# Create a kernel at the end of the time loop, every iteration (no frequency)
-		filter_class = UserDefinedEquations()
-		filter_class.algorithm_place = InTheSimulation(frequency=False)
-		filter_class.computation_name = 'Three point filter'
-		filter_class.add_equations(self.create_equations())
-		self.equation_classes.append(filter_class)
-		return
+    def create_filter(self):
+        # Create a kernel at the end of the time loop, every iteration (no frequency)
+        filter_class = UserDefinedEquations()
+        filter_class.algorithm_place = InTheSimulation(frequency=False)
+        filter_class.computation_name = 'Three point filter'
+        filter_class.add_equations(self.create_equations())
+        self.equation_classes.append(filter_class)
+        return
--- apps/cylinder/grid_generation/read_grid.py
+++ apps/cylinder/grid_generation/read_grid.py
@@ -10,11 +10,10 @@ data = np.genfromtxt('./Cyl-grid_Inc.dat')
 # nx, ny = data.shape[0,0], data.shape[0,1]
 nx, ny = 357, 179
 nhalo = 5 # 5 halos on each side of the domain
-print(nx, ny)
+print("Input grid size: (%d, %d)" % (nx, ny))
 # Reshape the data into 2D arrays
 output_x = data[:,0].reshape(nx,ny)
 output_y = data[:,2].reshape(nx,ny)
-print(output_y.shape)
 # Pad the data with zeros in the halos
 full_x, full_y = np.zeros((nx+2*nhalo, ny+2*nhalo)), np.zeros((nx+2*nhalo, ny+2*nhalo))
 full_x[nhalo:-(nhalo), nhalo:-(nhalo)] = output_x
@@ -22,20 +21,22 @@ full_y[nhalo:-(nhalo), nhalo:-(nhalo)] = output_y
 # # Copy coordinate data into the halos manually
 # Side 0
 # Fill out second index in both periodic condition
-print(full_x.shape)
+print("Grid with halos: (%d, %d)" % (full_x.shape[0], full_x.shape[1]))
 # X coordinate array
 # Left halos
-full_x[4,:] = full_x[nx-1, :]
-full_x[3,:] = full_x[nx-2, :]
-full_x[2,:] = full_x[nx-3, :]
-full_x[1,:] = full_x[nx-4, :]
-full_x[0,:] = full_x[nx-5, :]
-# # Right halos
-full_x[nx+5,:] = full_x[5,:]
-full_x[nx+6,:] = full_x[6,:]
-full_x[nx+7,:] = full_x[7,:]
-full_x[nx+8,:] = full_x[8,:]
-full_x[nx+9,:] = full_x[9,:]
+dx1 = np.abs(full_x[6,:] - full_x[5,:])
+full_x[4,:] = full_x[5,:] + dx1
+full_x[3,:] = full_x[5,:] + 2*dx1
+full_x[2,:] = full_x[5,:] + 3*dx1
+full_x[1,:] = full_x[5,:] + 4*dx1
+full_x[0,:] = full_x[5,:] + 5*dx1
+# Right halos
+dx2 = np.abs(full_x[nx+4,:] - full_x[nx+3,:])
+full_x[nx+5,:] = full_x[nx+3,:] + dx2
+full_x[nx+6,:] = full_x[nx+3,:] + 2*dx2
+full_x[nx+7,:] = full_x[nx+3,:] + 3*dx2 
+full_x[nx+8,:] = full_x[nx+3,:] + 4*dx2 
+full_x[nx+9,:] = full_x[nx+3,:] + 5*dx2 
 # Bottom halos
 full_x[:,4] = full_x[:, ny-1]
 full_x[:,3] = full_x[:, ny-2]
