611bda9f369df35491af9ab355de6e3fe0ee4259
David Lusher <dl11g11@soton.ac.uk>
2020-06-04 12:27:16 +0100

    Initialize all of the halo points

--- opensbli/initialisation/gridbasedinit.py
+++ opensbli/initialisation/gridbasedinit.py
@@ -9,6 +9,7 @@ from opensbli.core.kernel import Kernel
 from opensbli.core.opensbliobjects import GroupedPiecewise
 from sympy import Equality, flatten
 from opensbli.code_generation.algorithm.common import BeforeSimulationStarts
+from opensbli.schemes.spatial.scheme import CentralHalos_defdec
 
 
 class GridBasedInitialisation(NonSimulationEquations):
@@ -20,8 +21,7 @@ class GridBasedInitialisation(NonSimulationEquations):
             ret.order = 0
         ret.equations = []
         ret.kwargs = kwargs
-        """A control parameter is needed for where to put these equations in the algorithm
-        """
+        # A control parameter is needed for where to put these equations in the algorithm
         ret.algorithm_place = [BeforeSimulationStarts()]
         return ret
 
@@ -44,17 +44,16 @@ class GridBasedInitialisation(NonSimulationEquations):
         return evaluated
 
     def spatial_discretisation(cls, block):
-        kernel1 = Kernel(block, computation_name="Grid_based_initialisation%d" % cls.order)
-        kernel1.set_grid_range(block)
+        kernel = Kernel(block, computation_name="Grid_based_initialisation%d" % cls.order)
+        kernel.set_grid_range(block)
         schemes = block.discretisation_schemes
         for d in range(block.ndim):
-            for sc in schemes:
-                if schemes[sc].schemetype == "Spatial":
-                    kernel1.set_halo_range(d, 0, schemes[sc].halotype)
-                    kernel1.set_halo_range(d, 1, schemes[sc].halotype)
-        kernel1.add_equation(cls.equations)
-        kernel1.update_block_datasets(block)
-        cls.Kernels = [kernel1]
+            # Initialize all five halos
+            kernel.set_halo_range(d, 0, CentralHalos_defdec())
+            kernel.set_halo_range(d, 1, CentralHalos_defdec())
+        kernel.add_equation(cls.equations)
+        kernel.update_block_datasets(block)
+        cls.Kernels = [kernel]
         return
 
     def apply_boundary_conditions(cls, block):
